42d08aa009b9798513b94e9a2edbdaade48268bc
Allison Reid <root@cooltrainer.org>
2018-09-27 23:14:53 -0700

    Basic tag functionality and arg parsing

--- DistorteD-Jekyll/lib/cooltrainer/image.rb
+++ DistorteD-Jekyll/lib/cooltrainer/image.rb
@@ -1,7 +1,33 @@
 require "cooltrainer/image/version"
 
+require "liquid/tag/parser"
+
 module Cooltrainer
-  module Image
-    # Your code goes here...
+  class ImageTag < Liquid::Tag
+    def initialize(tag_name, arguments, liquid_options)
+      super
+      parsed_arguments = Liquid::Tag::Parser.new(arguments)
+      @image = parsed_arguments[:argv1]
+      @alt = parsed_arguments[:alt]
+      @title = parsed_arguments[:title]
+      @url = parsed_arguments[:url]
+      @caption = parsed_arguments[:caption]
+    end
+
+    def render(context)
+      template = Liquid::Template.parse(
+        File.read(File.join(File.dirname(__FILE__), "image.liquid"))
+      )
+      return template.render({
+        "image" => @image,
+        "alt" => @alt,
+        "title" => @title,
+        "url" => @url,
+        "caption" => @caption,
+      })
+    end
   end
 end
+
+Liquid::Template.register_tag('coolimage', Cooltrainer::ImageTag)
+
