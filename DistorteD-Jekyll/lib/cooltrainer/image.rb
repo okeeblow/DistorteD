5e97b7c73805fd3839f066d928bf2ccc73414587
Allison Reid <root@cooltrainer.org>
2019-10-09 01:18:16 -0700

    Refactor to actually work properly by only writing files in write() and rename variables to match superclass.

--- DistorteD-Jekyll/lib/cooltrainer/image.rb
+++ DistorteD-Jekyll/lib/cooltrainer/image.rb
@@ -35,63 +35,93 @@ module Jekyll
   class ImageFile < Jekyll::StaticFile
     def initialize(
         site,
-        site_source_path,
-        relative_original_image_directory,
-        destination_filename,
-        page_destination_directory,
-        image_pipeline,
-        dimensions
+        base,
+        dir,
+        name,
+        dest,
+        collection = nil
     )
-      # Construct a Jekyll::StaticFile for this image size, though we haven't
-      # yet generated the resized image at `destination_filename`.
+      @tag_name = self.class.name.split('::').last
+      Jekyll.logger.debug(@tag_name, "base is #{base}")
+      Jekyll.logger.debug(@tag_name, "dir is #{dir}")
+      Jekyll.logger.debug(@tag_name, "name is #{name}")
+      Jekyll.logger.debug(@tag_name, "dest is #{dest}")
+      @base = base
+      @dir = dir
+      @name = name
+      @dest = dest
+      # We will instantiate one Jekyll::StaticFile for each size generated
+      # from our original full-resolution image. One StaticFile instance
+      # tracks one actual file in the final `_site_ directory.
+      #
+      # Constructor args for Jekyll::StaticFile:
+      # site - The Jekyll Site object
+      # base - The String path to the generated `_site` directory.
+      # dir  - The String path for generated images, aka the page URL.
+      # name - The String filename for one generated or original image.
       super(
         site,
-        site_source_path,
-        relative_original_image_directory,
-        destination_filename
+        base,
+        dir,
+        name
       )
-      @destination_filename = destination_filename
-      @page_destination_directory = page_destination_directory
 
-      # Split String arg e.g. "153x426" dimensions into Int list e.g. [153, 426]
-      bounding_box = dimensions.split('x').map! { |x| Integer(x) }
+      dimensions = site.config['cool_image']['dimensions']
+
+      # Load original image into a processing pipeline to pass to each size
+      image_pipeline = ImageProcessing::Vips.source(
+        File.join(base, dir, name)
+      )
 
       # Resize to the given dimensions, and returning a Vips::Image
       # instead of saving over the original filename.
       resized_image = image_pipeline.resize_to_limit(
-        *bounding_box
+        200, 200
       ).call(save: false)
 
-      # We must use the [pre, post] Array argument to ensure the tempfile
-      # includes the same extension as the original file.
-      # Vips uses file extension to determine its saver format.
-      temp_file = Tempfile.new([
-        File.basename(destination_filename),
-        File.extname(destination_filename)
-      ])
-
       # Write the Vips::Image to the size-specific filename after resizing.
-      resized_image.write_to_file temp_file.path
-      Jekyll.logger.debug("ImageFile", "Wrote #{temp_file.path} #{bounding_box}")
-      @temp_file_path = temp_file.path
+      resized_name = File.join(dest, File.basename(name, ".*") + '-lol' + File.extname(name))
+      resized_file = File.new(resized_name, "w")
+      resized_file.close
+
+      resized_image.write_to_file resized_name
 
       # Tell Jekyll we modified this file so it will be included in the output.
       @modified = true
       @modified_time = Time.now
     end
 
+    # dest: string realpath to `_site_` directory
+    def destination(dest, suffix = nil)
+      if suffix
+        File.join(@dest, File.basename(@name, ".*") + '-' + suffix + File.extname(@name))
+      else
+        File.join(@dest, @name)
+      end
+    end
+
+    # dest: string realpath to `_site_` directory
+    def write(dest)
+      Jekyll.logger.debug("ImageFile", "Writing filez to #{dest}")
+      dest_path = destination(dest)
+      return false if File.exist?(dest_path) && !modified?
+
+      self.class.mtimes[path] = mtime
+
+      FileUtils.mkdir_p(File.dirname(dest_path))
+      FileUtils.rm(dest_path) if File.exist?(dest_path)
+      copy_file(dest_path)
+
+      true
+    end
+
     def modified?
       return true
     end
 
-    def path
-      Jekyll.logger.debug("ImageFile", "Using temp file #{@temp_file_path}")
-      return @temp_file_path
-    end
-
-    def destination(dest)
-      File.join(@page_destination_directory, @destination_filename)
-    end
+    #def destination(dest)
+    #  File.join(@destdir, @name)
+    #end
   end
 
   class CooltrainerImage < Liquid::Tag
@@ -110,27 +140,40 @@ module Jekyll
 
       # Parse arguments with https://github.com/envygeeks/liquid-tag-parser
       parsed_arguments = Liquid::Tag::Parser.new(arguments)
-      @original_image_filename = parsed_arguments[:argv1]
+
+      # name - original image filename
+      # alt - String contents of <img alt>
+      # title - String contents of <img title>
+      # caption - String displayed under image in template.
+      @name = parsed_arguments[:argv1]
       @alt = parsed_arguments[:alt]
       @title = parsed_arguments[:title]
       @url = parsed_arguments[:url]
       @caption = parsed_arguments[:caption]
     end
 
+    # This will become render_to_output_buffer(context, output) some day,
+    # according to upstream Liquid tag.rb.
     def render(context)
-      # Pull Jekyll site object back from context registers because we need
-      # to pass it to the ImageFile created for each size.
+      # Get Jekyll Site object back from tag rendering context registers so we
+      # can get configuration data and path information from it,
+      # then pass it along to our StaticFile subclass.
       site = context.registers[:site]
 
-      # Get image size name/dimension hash from config.
+      # We need a String path for site source, not Pathname, for StaticFile.
+      @source = Pathname.new(site.source).to_path
+
+      # Get image dimension configuration data.
+      # TODO: Defaults? What should this tag do with no config?
       # Example:
-      # {
-      #   "thumbnail"=>"400x250",
-      #   "medium"=>"800x500",
-      #   "large"=>"1200x750"
-      # }
-      sizes = site.config['cool_image']['sizes']
-      Jekyll.logger.debug(@tag_name, sizes)
+      # [
+      #   {:name=>"thumbnail", width=>400, height=>250, media=>""},
+      # ]
+
+      # TODO: Handle failure when config block is missing
+      dimensions = site.config['cool_image']['dimensions']
+      # TODO: Conditional debug since even that is spammy with many tags.
+      Jekyll.logger.debug(@tag_name, dimensions)
 
       # Access context data for the page including this tag.
       # Jekyll fills the first `page` Liquid context variable with the complete
@@ -145,22 +188,9 @@ module Jekyll
       markdown_pathname = Pathname.new(page_data["path"])
       Jekyll.logger.debug(
         @tag_name,
-        "Initializing for #{@original_image_filename} in #{markdown_pathname}"
+        "Initializing for #{@name} in #{markdown_pathname}"
       )
-
-      # Get the complete path to the original image file we're resizing.
-      # This must be a realpath because it gets compared to the built-in
-      # Jekyll::Site.source with `relative_path_from` which requires a realpath.
-      original_image_pathname = markdown_pathname.realpath.dirname + @original_image_filename
-
-      # Test (again) that the original image exists and bail out if not.
-      if FileTest.exist?(original_image_pathname)
-        Jekyll.logger.debug(@tag_name, "#{original_image_pathname} exists")
-        @original_image_pathname = original_image_pathname
-      else
-        Jekyll.logger.error(@tag_name, "#{original_image_pathname} does not exist")
-        raise ImageNotFoundError.new(original_image_pathname)
-      end
+      @srcdir = markdown_pathname.realpath.dirname
 
       # Access the final generated URL of the page including this tag,
       # relative to the directory of the generated site.
@@ -170,42 +200,33 @@ module Jekyll
       # and these directories are where we want to put our images.
       #
       # Example: 2019-06-22-laundry-day.markdown has `url` /laundry-day/
-      page_destination_directory = site.dest + page_data["url"]
+      dest = site.dest + page_data["url"]
       Jekyll.logger.debug(
         @tag_name,
-        "Generated images will be placed in #{page_destination_directory}"
+        "Generated images will be placed in #{dest}"
       )
 
-      # We need a String path for site source, not Pathname.
-      site_source_path = Pathname.new(site.source).to_path
-
       # Relative path from site source dir to original image's parent dir
-      relative_original_image_path = original_image_pathname.relative_path_from(
+      dir = Pathname(@srcdir + @name).relative_path_from(
         Pathname.new(site.source)
       ).dirname.to_path
 
-      # Load original image into a processing pipeline to pass to each size
-      image_pipeline = ImageProcessing::Vips.source(
-        original_image_pathname.to_path
-      )
-
-      # Generate a StaticFile for each size
-      generated = sizes.map {
-        |size_name, dimensions|
-        ImageFile.new(
-          site,
-          site_source_path,
-          relative_original_image_path,
-          image_name(@original_image_filename, size_name),
-          page_destination_directory,
-          image_pipeline,
-          dimensions
-        )
-      }
-
       # Tell Jekyll about the files we just created
-      site.static_files.concat(generated)
       # TODO: Copy the original file too if we don't have jekyll-postfiles
+      # StaticFile args:
+      # site - The Site.
+      # base - The String path to the <source> - /srv/jekyll
+      # dir  - The String path between <source> and the file - _posts/somedir
+      # name - The String filename of the file - cool.svg
+      # dest - The String path to the containing folder of the document which is output
+      base = Pathname.new site.source
+      site.static_files << ImageFile.new(
+        site,
+        base,
+        dir,
+        @name,
+        dest
+      )
 
       template = Liquid::Template.parse(
         File.read(File.join(File.dirname(__FILE__), "image.liquid"))
@@ -217,17 +238,6 @@ module Jekyll
         "url" => @url,
         "caption" => @caption,
       })
-    end
-
-    # Generate a size-specific filename for an image.
-    # Ex: "tidd.png" for setting "large"=>"1200x750" = "tidd-large.png"
-    def image_name(image_filename, size_name)
-      return [
-        File.basename(image_filename, ".*"),
-        "-",
-        size_name,
-        File.extname(image_filename)
-      ].join()
     end
   end
 end
