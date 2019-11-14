6a5bb44309aed983b179ef2e614c847e7093831d
Allison Reid <root@cooltrainer.org>
2019-11-14 14:39:13 -0800

    Move all-format pathfinding code to Invoker#render

--- DistorteD-Jekyll/lib/distorted-jekyll.rb
+++ DistorteD-Jekyll/lib/distorted-jekyll.rb
@@ -61,6 +61,72 @@ module Jekyll
         instance_variable_set('@' + attr.to_s, parsed_arguments[attr])
       end
     end
+
+    def render(context)
+      # Get Jekyll Site object back from tag rendering context registers so we
+      # can get configuration data and path information from it,
+      # then pass it along to our StaticFile subclass.
+      site = context.registers[:site]
+
+      # We need a String path for site source, not Pathname, for StaticFile.
+      @source = Pathname.new(site.source).to_path
+
+      # Access context data for the page including this tag.
+      # Jekyll fills the first `page` Liquid context variable with the complete
+      # text content of the page Markdown source, and page variables are
+      # available via Hash keys, both for generated options like `path`
+      # as well as options explicitly defined in the Markdown front-matter.
+      page_data = context.environments.first['page']
+
+      # Extract the pathname of the Markdown source file
+      # of the page including this tag, relative to the site source directory.
+      # Example: _posts/2019-04-20/laundry-day-is-a-very-dangerous-day.markdown
+      markdown_pathname = Pathname.new(page_data['path'])
+      Jekyll.logger.debug(
+        @tag_name,
+        "Initializing for #{@name} in #{markdown_pathname}"
+      )
+      @srcdir = markdown_pathname.realpath.dirname
+
+      # Generate image destination based on URL of the page invoking this tag,
+      # relative to the directory of the generated site.
+      # This URL can be explicitly defined in the page's Markdown front-matter,
+      # otherwise automatically generated based on the `permalink` config.
+      # Assume these paths will only ever be directories containing an index.html,
+      # and that these directories are where we want to put our images.
+      #
+      # Example:
+      # A post 2019-06-22-laundry-day.markdown has `url` /laundry-day/ based
+      # on my _config.yml setting "permalink: /:title/",
+      # so any images displayed in a {% distorted %} tag on that page will end
+      # up in the generated path `_site/laundry-day/`.
+      @url = page_data['url']
+
+      # Relative path from site source dir to original image's parent dir
+      dir = Pathname(@srcdir + @name).relative_path_from(
+        Pathname.new(site.source)
+      ).dirname.to_path
+
+      # Tell Jekyll about the files we just created
+      #
+      # StaticFile args:
+      # site - The Site.
+      # base - The String path to the <source> - /home/okeeblow/cooltrainer
+      # dir  - The String path between <base> and the file - _posts/2018-10-15-super-cool-post
+      # name - The String filename - cool.jpg
+      #
+      # Our subclass' additional args:
+      # dest - The String path to the generated `url` folder of the page HTML output
+      base = Pathname.new site.source
+      site.static_files << Jekyll::DistorteD::ImageFile.new(
+        site,
+        base,
+        dir,
+        @name,
+        @url,
+      )
+    end
+
   end
 end
 
