ea4042b05eb2ad46ea9c48c585fea77b1ced9159
Allison Reid <root@cooltrainer.org>
2019-11-15 01:11:26 -0800

    Use a pre_render hook to transform Markdown image declarations to DistorteD tags.

--- DistorteD-Jekyll/lib/distorted-jekyll.rb
+++ DistorteD-Jekyll/lib/distorted-jekyll.rb
@@ -1,5 +1,10 @@
+require 'injection_of_love'
 require 'invoker'
 
 # Register DistorteD's entrypoint class with Liquid.
 # `Invoker` will mix in the proper handler module for the given media.
 Liquid::Template.register_tag('distorted', Jekyll::DistorteD::Invoker)
+
+# Transform Markdown image syntax ![alt](url.jpg "title")
+# to instances of our liquid tag {% distorted %}
+Jekyll::Hooks.register(:documents, :pre_render, &distort_markdown)
