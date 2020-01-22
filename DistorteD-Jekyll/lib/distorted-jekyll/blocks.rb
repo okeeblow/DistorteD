4f700c93562ba8e708c41427e41d1b418329de3d
Allison Reid <root@cooltrainer.org>
2020-01-21 17:29:03 -0800

    Entry point for multi-instance DD block.

---
+++ DistorteD-Jekyll/lib/distorted-jekyll/blocks.rb
@@ -0,0 +1,19 @@
+require 'distorted/floor'
+
+# Do I need https://stackoverflow.com/questions/36945321/nested-liquid-custom-tag-blocks ?
+
+module Jekyll
+  class BLOCKS < Liquid::Block
+
+    include Jekyll::DistorteD::Floor
+
+    def initialize(tag_name, arguments, liquid_options)
+      super
+    end
+
+    def render(context)
+      "<div class=\"distorted-block\">#{super}</div>"
+    end
+
+  end
+end
