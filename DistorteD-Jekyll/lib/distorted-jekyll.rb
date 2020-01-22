a74d30ea39d8b0c4e26270381d12a68ea3601f6a
Allison Reid <root@cooltrainer.org>
2020-01-21 17:29:03 -0800

    Entry point for multi-instance DD block.

--- DistorteD-Jekyll/lib/distorted-jekyll.rb
+++ DistorteD-Jekyll/lib/distorted-jekyll.rb
@@ -1,9 +1,13 @@
+require 'blocks'
 require 'injection_of_love'
 require 'invoker'
 
 # Register DistorteD's entrypoint class with Liquid.
 # `Invoker` will mix in the proper handler module for the given media.
 Liquid::Template.register_tag('distorted', Jekyll::DistorteD::Invoker)
+
+# Register a block version for arranging multiple pieces of media.
+Liquid::Template.register_tag('distort', Jekyll::BLOCKS)
 
 # Transform Markdown image syntax ![alt](url.jpg "title")
 # to instances of our liquid tag {% distorted %}
