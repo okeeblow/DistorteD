553ec04a05c02e0af9933c48f22161c1a8d20e7e
Allison Reid <root@cooltrainer.org>
2020-02-05 02:08:04 -0800

    Expand regex to commented syntax and start using a custom Kramdown converter.

--- DistorteD-Jekyll/lib/distorted-jekyll/md_injection.rb
+++ DistorteD-Jekyll/lib/distorted-jekyll/md_injection.rb
@@ -6,6 +6,82 @@ require 'kramdown'
 # so we can't rely on customizing the Markdown renderer's `:img` element
 # output as a means to invoke DistorteD.
 # https://jekyllrb.com/tutorials/orderofinterpretation/
+#
+# Prefer the POSIX-style bracket expressions (e.g. [[:digit:]]) over
+# basic character classes (e.g. \d) because they match Unicode
+# instead of just ASCII.
+# https://ruby-doc.org/core/Regexp.html#class-Regexp-label-Character+Classes
+#
+# Support additional arguments passed to DistorteD via Kramdown-style
+# inline attribute lists.
+# https://kramdown.gettalong.org/syntax.html#images
+# https://kramdown.gettalong.org/syntax.html#inline-attribute-lists
+#
+# IALs can be on the same line or on a line before or after their
+# associated flow element.
+# In ambiguous situations (flow elements both before and after an IAL),
+# the IAL applies to the flow element before it.
+# All associated elements and IALs must be on contiguous lines.
+#
+# This page provides a regex for parsing IALs, Section 5.3:
+# https://golem.ph.utexas.edu/~distler/maruku/proposal.html
+MD_IMAGE_REGEX = %r&
+  # Image container
+  (
+    # Preceding-line attribute list
+    (
+      # One blank line with newline via :space:
+      ^$[[:space:]]
+      # Any amount of blank space before AL
+      [[:blank:]]*
+      # IAL regex from Section 5.3:
+      # https://golem.ph.utexas.edu/~distler/maruku/proposal.html
+      (?<pre_al>\{:(\\\}|[^\}])*\})
+      # Any amount of trailing whitespace followed by a newline.
+      [[:blank:]]*[[:space:]]
+    )?  # Match all of that, or nothing.
+    # Eat list items containing images since that's the syntax
+    # I decided to use for media block arrangements.
+    (
+      # Include both unordered (-+*) and ordered (\d\. like `1.`) lists.
+      # Support any amount of leading whitespace and one space/tab
+      # between list delimiter and the image.
+      [[:blank:]]*[-\*\+|\d\.][[:blank:]]
+      # Support an optional IAL for the list element as shown in Kramdown docs:
+      # https://kramdown.gettalong.org/syntax.html#lists
+      # Ctrl+F "{:.cls}"
+      (\{:(\\\}|[^\}])*\})?
+    )?  # List items are optional!
+    # Match Markdown image syntax:
+    #   ![alt text](/some/path/to/image.png 'title text'){:other="options"}
+    !\[(?<alt>(\\\]|[^\]])*)\]
+    # Match img src as anything after the '(' and before ')' or
+    # anything that could be a title.
+    # Assume titles will be quoted.
+    \((?<src>[^'")]+)
+    # Title is optional.
+    # Don't including the opening or closing quotes in the capture.
+    (['"](?<title>[^'"]*)['"])?
+    # The closing ')' will always be present.
+    \)
+    # IALs may be on the same line as the image after some amount of whitespace.
+    ([[:blank:]]*(?<inline_al>\{:(\\\}|[^\}])*\}))*
+    # Also support optional IALs on the line following the image.
+    # :space: matches newlines regardless of encoding.
+    ([[:space:]][[:blank:]]*(?<post_al>\{:(\\\}|[^\}])*\})*)?
+  )+  # Capture multiple images together for block display.
+&x
+
+def extract_list(hash, collect = false)
+  hash.map do |k, v|
+    v.is_a?(Array) ? extract_list(v, (k == :children)) : 
+       (collect ? v : nil)
+  end.compact.flatten
+end
+
+def get_images(hash, collection = nil)
+  hash[:children]
+end
 
 def distort_markdown
   Proc.new { |document, payload|
@@ -20,31 +96,20 @@ def distort_markdown
       # effectively a bad idea lol) but it's the cleanest way I can come up
       # with right now for separating DistorteD-destined Markdown from
       # the rest of any given page.
-      #
-      # Prefer the POSIX-style bracket expressions (e.g. [[:digit:]]) over
-      # basic character classes (e.g. \d) because they match Unicode
-      # instead of just ASCII.
-      # https://ruby-doc.org/core/Regexp.html#class-Regexp-label-Character+Classes
-      #
-      # Support additional arguments passed to DistorteD via Kramdown-style
-      # inline attribute lists.
-      # https://kramdown.gettalong.org/syntax.html#images
-      # https://kramdown.gettalong.org/syntax.html#inline-attribute-lists
-      #
-      # IALs can be on the same line or on a line before or after their
-      # associated flow element.
-      # In ambiguous situations (flow elements both before and after an IAL),
-      # the IAL applies to the flow element before it.
-      # All associated elements and IALs must be on contiguous lines.
-      #
-      # ![alt text](filename.png 'Title text'){:other="options"}
-      # or
-      # ![Eh](hello.jpg 'Receiver')
-      # {:ial="is on a new line!"}
-      document.content = document.content.gsub(
-        # Thank you for being here to share my pain.
-        /((^$\n[[:blank:]]*(?<pre_al>{:[^}]+})\n[[:blank:]]*)?([[:blank:]]*[-*+|\d\.][[:blank:]])?!\[(?<alt>.*)\]\((?<name>[^'")]+)(['"](?<title>[^'")]+)['"])?\)(?<inline_al>{:([^}]+)})*(\n[[:blank:]]*(?<post_al>{:([^}]+)})*)?)+/,
-      ) { |match| Kramdown::Document.new(match).to_hash_ast }
+      document.content = document.content.gsub(MD_IMAGE_REGEX) { |match| 
+        Kramdown::Document.new(match).to_liquid
+      }
     end
   }
+end
+
+module Kramdown
+
+  module Converter
+
+    # Converts a Kramdown::Document to a nested hash for further processing or debug output.
+    class Liquid < Base
+    end
+
+  end
 end
