1622153e34308770126f238a634507e62b2cca01
Allison Reid <root@cooltrainer.org>
2020-02-05 20:56:28 -0800

    Rename autogenerated test harness to match the current state of the gem.

--- DistorteD-Floor/test/distorted_test.rb
+++ DistorteD-Floor/test/distorted_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class Cooltrainer::ImageTest < Minitest::Test
+class Cooltrainer::DistorteDTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Cooltrainer::Image::VERSION
+    refute_nil ::Cooltrainer::DistorteD::VERSION
   end
 
   def test_it_does_something_useful
