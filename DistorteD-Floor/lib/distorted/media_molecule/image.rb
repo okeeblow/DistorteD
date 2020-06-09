4b31bb2c7123bf1bfa07e719480c1276595dca06
Allison Reid <root@cooltrainer.org>
2020-06-09 15:15:04 -0700

    Move image file handling code out of the Jekyll-specific gem.

---
+++ DistorteD-Floor/lib/distorted/media_molecule/image.rb
@@ -0,0 +1,61 @@
+# Tell the user to install the shared library if it's missing.
+begin
+  require 'vips'
+rescue LoadError => le
+  # Only match libvips.so load failure
+  raise unless le.message =~ /libvips.so/
+
+  # Multiple OS help
+  help = <<~INSTALL
+
+  Please install the libvips image processing library.
+
+  FreeBSD:
+    pkg install graphics/vips
+
+  macOS:
+    brew install vips
+
+  Debian/Ubuntu/Mint:
+    apt install libvips libvips-dev
+  INSTALL
+
+  # Re-raise with install message
+  raise $!, "#{help}\n#{$!}", $!.backtrace
+end
+
+module Cooltrainer
+  class DistorteD
+    class Image
+
+      attr_accessor :dimensions
+
+      def initialize(src)
+        @image = Vips::Image.new_from_file(src)
+      end
+
+      def rotate(angle=nil)
+        if angle == :auto
+          @image = @image.autorot
+        end
+      end
+
+      def clean
+        # Nuke the entire site from orbit. It's the only way to be sure.
+        @image.get_fields.grep(/exif-ifd/).each {|field| @image.remove field}
+      end
+
+      def write
+        for d in @dimensions
+          if d[:tag] == :full
+            @image.write_to_file(d[:dest])
+          else
+            ver = @image.thumbnail_image(d[:width], {:crop => d[:crop]})
+            ver.write_to_file(d[:dest])
+          end
+        end
+      end
+
+    end  # Image
+  end  # DistorteD
+end  # Cooltrainer
--- DistorteD-Jekyll/lib/distorted-jekyll/static/image.rb
+++ DistorteD-Jekyll/lib/distorted-jekyll/static/image.rb
@@ -1,29 +1,4 @@
-# Tell the user to install the shared library if it's missing.
-begin
-  require 'vips'
-rescue LoadError => le
-  # Only match libvips.so load failure
-  raise unless le.message =~ /libvips.so/
-
-  # Multiple OS help
-  help = <<~INSTALL
-
-  Please install the libvips image processing library.
-
-  FreeBSD:
-    pkg install graphics/vips
-
-  macOS:
-    brew install vips
-
-  Debian/Ubuntu/Mint:
-    apt install libvips libvips-dev
-  INSTALL
-
-  # Re-raise with install message
-  raise $!, "#{help}\n#{$!}", $!.backtrace
-end
-
+require 'distorted/image'
 require 'formats/static_state'
 
 module Jekyll
@@ -45,31 +20,36 @@ module Jekyll
 
     # dest: string realpath to `_site_` directory
     def write(dest)
-      orig_dest = destination(dest)
+      fullres_dest = destination(dest)
       return false if File.exist?(orig_path) && !modified?
 
       self.class.mtimes[path] = mtime
 
-      FileUtils.mkdir_p(File.dirname(orig_dest))
-      FileUtils.rm(orig_dest) if File.exist?(orig_dest)
+      FileUtils.mkdir_p(File.dirname(fullres_dest))
+      FileUtils.rm(fullres_dest) if File.exist?(fullres_dest)
 
-      orig = Vips::Image.new_from_file orig_path
+      distorted = Cooltrainer::DistorteD::Image.new(orig_path)
 
-      Jekyll.logger.info(@tag_name, "Crop setting is #{@crop}")
       Jekyll.logger.debug(@tag_name, "Rotating #{@name} if tagged.")
-      orig = orig.autorot
+      distorted.rotate(:auto)
 
-      # Nuke the entire site from orbit. It's the only way to be sure.
-      orig.get_fields.grep(/exif-ifd/).each {|field| orig.remove field}
 
-      orig.write_to_file orig_dest
+      dimensions = [{:tag => :full, :dest => fullres_dest}]
 
       for d in @dimensions
-        ver_dest = destination(dest, d['tag'])
-        Jekyll.logger.debug(@tag_name, "Writing #{d['width']}px version to #{ver_dest}")
-        ver = orig.thumbnail_image(d['width'], {:crop => @crop&.to_sym || :attention})
-        ver.write_to_file ver_dest
+        dimension = {
+          :width => d['width'],  # Convert to AR-aware height-fit
+          :crop => @crop&.to_sym || :attention,
+          :tag => d['tag'].to_sym,
+          :dest => destination(dest, d['tag'].to_sym),
+        }
+        Jekyll.logger.debug(@tag_name, "Adding dimension #{dimension}")
+        dimensions.append(dimension)
       end
+
+      distorted.dimensions = dimensions
+
+      distorted.write
 
       true
     end
