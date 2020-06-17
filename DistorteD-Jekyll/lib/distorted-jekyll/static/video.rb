e2984f2bcb17018618f4d1fc620a2aa3e20edf70
Allison Reid <root@cooltrainer.org>
2020-06-17 11:07:08 -0700

    Add a Video#modified? method to speed up site generation for our hacky prototype implementation while waiting on Gst features

--- DistorteD-Jekyll/lib/distorted-jekyll/static/video.rb
+++ DistorteD-Jekyll/lib/distorted-jekyll/static/video.rb
@@ -20,7 +20,26 @@ module Jekyll
           distorted.generate
         end
 
-      end
-    end
-  end
-end
+        def modified?
+          # We can't use the standard Static::State#modified? here until
+          # I figure out how to cleanly get a duplicate of what would be
+          # the generated filenames from GStreamer's sink.
+          # For now for the sake of speeding up my site generation
+          # I'll assume not-modified that if the output variant (e.g. DASH/HLS)
+          # container dir exists and contains at least two files:
+          # the playlist and at least one segment.
+          # Hacky HLS-only right now until dashsink2 lands in upstream Gst.
+          var_dir = "#{@dir}.hls"
+          if Dir.exist?(@dir)
+            need_filez = Set["#{basename}.m3u8"]
+            var_filez = Dir.entries(@dir).to_set
+            mod = need_filez.subset?(var_filez) and var_filez.count > 2
+            Jekyll.logger.debug("#{@name} modified?",  mod)
+            return mod
+          end
+        end
+
+      end  # Video
+    end  # Static
+  end  # DistorteD
+end  # Jekyll
