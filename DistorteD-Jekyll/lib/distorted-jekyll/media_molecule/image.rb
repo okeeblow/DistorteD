9713cdef6640ec87205bfe9dbd9f251699acb29b
Allison Reid <root@cooltrainer.org>
2020-06-17 10:16:35 -0700

    Refactor Molecule::Image to live in the proper module. Also support lazy loading because Git doesn't want to let me split that part  out and still generate a working changeset.

--- DistorteD-Jekyll/lib/distorted-jekyll/media_molecule/image.rb
+++ DistorteD-Jekyll/lib/distorted-jekyll/media_molecule/image.rb
@@ -3,48 +3,63 @@ require 'distorted-jekyll/static/image'
 
 module Jekyll
   module DistorteD
-    module Image
+    module Molecule
+      module Image
 
-      MEDIA_TYPE = Cooltrainer::DistorteD::Image::MEDIA_TYPE
-      MIME_TYPES = Cooltrainer::DistorteD::Image::MIME_TYPES
-      ATTRS = Set[:alt, :caption, :href, :crop]
-      CONFIG_SUBKEY = :image
+        # Spice up our singleton.
+        include Jekyll::DistorteD::Molecule::C18H27NO3;
 
-      # This will become render_to_output_buffer(context, output) some day,
-      # according to upstream Liquid tag.rb.
-      def render(context)
-        super
-        begin
-          parse_template.render({
-            'name' => @name,
-            'path' => @url,
-            'alt' => @alt,
-            'title' => @title,
-            'href' => @href,
-            'caption' => @caption,
-            'sources' => sources,
-          })
-        rescue Liquid::SyntaxError => l
-          # TODO: Only in dev
-          l.message
+        # Reference these instead of reassigning them. Consistency is mandatory.
+        MEDIA_TYPE = Cooltrainer::DistorteD::Image::MEDIA_TYPE
+        MIME_TYPES = Cooltrainer::DistorteD::Image::MIME_TYPES
+
+        ATTRS = Cooltrainer::DistorteD::Image::ATTRS
+        ATTRS_DEFAULT = Cooltrainer::DistorteD::Image::ATTRS_DEFAULT
+        ATTRS_VALUES = Cooltrainer::DistorteD::Image::ATTRS_VALUES
+
+        CONFIG_SUBKEY = :image
+
+
+        # https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#attr-loading
+        def loading
+          # These have been mixed in to the singleton class already for us to be able to get here.
+          values = ATTRS_VALUES
+
+          # The instance var is set in Invoker when the molecule is mixed in.
+          if values[:loading].include?(@loading)
+            @loading
+          else
+            values[:loading].to_s
+          end
         end
-      end
 
-      def dimensions
-        config(CONFIG_SUBKEY)
-      end
 
-      def sources
-        dimensions.map { |d| {
-          'name' => name(d[:tag]),
-          'media' => d[:media]
-        }}
-      end
+        # This will become render_to_output_buffer(context, output) some day,
+        # according to upstream Liquid tag.rb.
+        def render(context)
+          super
+          begin
+            parse_template.render({
+              'name' => @name,
+              'path' => @url,
+              'alt' => @alt,
+              'title' => @title,
+              'href' => @href,
+              'caption' => @caption,
+              'loading' => loading,
+              'filenames' => @filenames,
+            })
+          rescue Liquid::SyntaxError => l
+            # TODO: Only in dev
+            l.message
+          end
+        end
 
-      def static_file(site, base, dir, name, url)
-        Jekyll::DistorteD::ImageFile.new(site, base, dir, name, url)
-      end
+        def static_file(site, base, dir, name, url, dimensions, types, filenames)
+          Jekyll::DistorteD::Static::Image.new(site, base, dir, name, url, dimensions, types, filenames)
+        end
 
+      end
     end
   end
 end
