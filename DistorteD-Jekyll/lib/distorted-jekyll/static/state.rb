96a8f479e36d3f555cc2cc7dd0a6d5a186eeb7f3
Allison Reid <root@cooltrainer.org>
2020-06-17 09:51:24 -0700

    Refactor StaticFile base class to not pollute the 'Jekyll' namespace alongside the actual Jekyll::StaticFile. Also rename orig_path -> src_path for consistency with the many-types output model.

--- DistorteD-Jekyll/lib/distorted-jekyll/static/state.rb
+++ DistorteD-Jekyll/lib/distorted-jekyll/static/state.rb
@@ -1,51 +1,57 @@
 
 module Jekyll
-  # Tag-specific StaticFile child that handles thumbnail generation.
-  class StaticState < Jekyll::StaticFile
+  module DistorteD
+    module Static
+      class State < Jekyll::StaticFile
 
-    def initialize(
-      site,
-      base,
-      dir,
-      name,
-      url,
-      collection = nil
-    )
-      @tag_name = self.class.name.split('::').drop(1).join('::')
-      Jekyll.logger.debug(@tag_name, "#{base}/#{dir}/#{name} -> #{url}")
-      @base = base
-      @dir = dir
-      @name = name
-      @url = url
+        def initialize(
+          site,
+          base,
+          dir,
+          name,
+          url,
+          collection = nil
+        )
+          @base = base
+          @dir = dir
+          @name = name
+          @url = url
 
-      # Child classes need clean versions of these for various things.
-      @basename = File.basename(name, '.*')
-      @extname = File.extname(name)
+          # Constructor args for Jekyll::StaticFile:
+          # site - The Jekyll Site object
+          # base - The String path to the generated `_site` directory.
+          # dir  - The String path for generated images, aka the page URL.
+          # name - The String filename for one generated or original image.
+          super(
+            site,
+            base,
+            dir,
+            name
+          )
+        end
 
-      # Constructor args for Jekyll::StaticFile:
-      # site - The Jekyll Site object
-      # base - The String path to the generated `_site` directory.
-      # dir  - The String path for generated images, aka the page URL.
-      # name - The String filename for one generated or original image.
-      super(
-        site,
-        base,
-        dir,
-        name
-      )
-    end
+        def basename
+          File.basename(@name, '.*')
+        end
 
-    # site_dest: string realpath to `_site_` directory
-    def destination(dest, suffix = nil)
-      File.join(dest, @url, Jekyll::DistorteD::Floor::image_name(@name))
-    end
+        def extname
+          File.extname(@name)
+        end
 
-    def modified?
-      return true
-    end
+        # site_dest: string realpath to `_site_` directory
+        def destination(dest, tag = nil, extension = nil)
+          File.join(dest, @url, filename(basename, tag, extension))
+        end
 
-    def orig_path
-      File.join(@base, @dir, @name)
-    end
-  end
-end
+        def modified?
+          return true
+        end
+
+        def src_path
+          File.join(@base, @dir, @name)
+        end
+
+      end  # state
+    end  # Static
+  end  # DistorteD
+end  # Jekyll
