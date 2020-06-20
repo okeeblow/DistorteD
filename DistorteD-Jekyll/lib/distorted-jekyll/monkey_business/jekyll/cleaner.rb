0c1a4a1fe51e6c66f0490c5c47eb27a30890f9ab
Allison Reid <root@cooltrainer.org>
2020-06-20 15:01:26 -0700

    Integrate DD with Jekyll::Cleaner using a custom new_files implementation that looks for StaticFiles that respond to :destinations instead of just Jekyll's original :destination. Add an implementation of Static::State.destinations using the passed-in DD variations filenames.

---
+++ DistorteD-Jekyll/lib/distorted-jekyll/monkey_business/jekyll/cleaner.rb
@@ -0,0 +1,46 @@
+module Jekyll
+  # Handles the cleanup of a site's destination before it is built or re-built.
+  class Cleaner
+
+    # Private: The list of files to be created when site is built.
+    #
+    # Returns a Set with the file paths
+    #
+    # Monkey-patch this to look for DD's unique `destinations` which is similar
+    # to the original `destination` method except it returns a Set of destination
+    # paths instead of a single destination path.
+    # Do the patch with `define_method` instead of just `def` because the block's
+    # closure of the local scope lets it carry a binding to the original overriden
+    # method which I use to bail out iff the monkey-patch fails.
+    # This is an attempt to avoid breaking future Jekyll versions as much as
+    # possible, since any Exception in the monkey-patched code will just cause
+    # the original Jekyll implementation to be called instead.
+    # The new worst case scenario is slow site builds due to media variation generation!
+    #
+    # If a StaticFile responds to `destinations` then use it and merge the result.
+    # I'm defining my own separate method for multi-destinations for now,
+    # but I also considered just overriding `destination` to return the Set and
+    # then doing this as a one-liner that handles either case (single or
+    # multiple destinations) with `files.merge(Set[*(item.destination(site.dest))])`
+    the_old_new_thing = instance_method(:new_files)
+    define_method(:new_files) do
+      begin
+        @new_files ||= Set.new.tap do |files|
+          site.each_site_file { |item|
+            if item.respond_to?(:destinations)
+              files.merge(item.destinations(site.dest))
+            else
+              files << item.destination(site.dest)
+            end
+          }
+        end
+      rescue Exception
+        # Explicitly rescuing from Exception instead of StandardError because
+        # I want to catch SyntaxError and friends too.
+        Jekyll.logger.warn('DistorteD', "Monkey-patching Jekyll::Cleaner#new_files failed: #{e.message}")
+        Jekyll.logger.debug('DistorteD', "Monkey-patched Jekyll::Cleaner#new_files backtrace: #{e.backtrace}")
+        the_old_new_thing.bind(self).()
+      end
+    end
+  end  # Cleaner
+end  # Jekyll
