71ffb9adbb97a15c23152f8cdd0b4ada1269f909
Allison Reid <root@cooltrainer.org>
2020-06-22 16:38:20 -0700

    🐒🩹 Hash.to_set to complement Ruby::YAML's Set implementation. Only affects Hashes with all-nil values:
      irb(main):066:0> lol
      => #<Set: {1, 2, 3}>
    
      irb(main):067:0> rofl
      => {:a=>nil, :b=>nil, :c=>nil}
    
      irb(main):068:0> lmao
      => {:a=>1, :b=>2, :c=>3}
    
      irb(main):069:0> lol.to_h
      => {1=>nil, 2=>nil, 3=>nil}
    
      irb(main):070:0> rofl.to_set
      => #<Set: {:a, :b, :c}>
    
      irb(main):071:0> lmao.to_set
      => #<Set: {[:a, 1], [:b, 2], [:c, 3]}>

--- DistorteD-Floor/lib/distorted-floor/monkey_business/hash.rb
+++ DistorteD-Floor/lib/distorted-floor/monkey_business/hash.rb
@@ -1,7 +1,20 @@
-# https://github.com/dam13n/ruby-bury/blob/master/hash.rb
-# This is not packaged as a Gem or I'd be using it instead of including my own.
 class Hash
+
+  # Complement Ruby::YAML behavior, where usage of Set syntax
+  # returns a Hash with all-nil values.
+  # Calling :to_set on a Hash with all-nil values should return
+  # a Set of the Hash's keys.
+  this_old_set = instance_method(:to_set)
+  define_method(:to_set) do
+    if self.values.all?{ |v| v.nil? }
+      self.keys.to_set
+    else
+      this_old_set.bind(self).()
+    end
+  end
   
+  # https://github.com/dam13n/ruby-bury/blob/master/hash.rb
+  # This is not packaged as a Gem or I'd be using it instead of including my own.
   def bury(*args)
     if args.count < 2
       raise ArgumentError.new('2 or more arguments required')
