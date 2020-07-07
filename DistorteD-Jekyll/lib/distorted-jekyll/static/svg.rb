871c479e0f9751c262dadddd815b238ee37612df
Allison Reid <root@cooltrainer.org>
2020-07-06 19:11:09 -0700

    Support SvgOptimizer! For now always enable all optimizations when writing an SVG.

--- DistorteD-Jekyll/lib/distorted-jekyll/static/svg.rb
+++ DistorteD-Jekyll/lib/distorted-jekyll/static/svg.rb
@@ -1,6 +1,8 @@
 require 'fileutils'
 require 'set'
 
+require 'svg_optimizer'
+
 require 'distorted-jekyll/static/image'
 
 module Jekyll
@@ -16,8 +18,16 @@ module Jekyll
           # Create any directories to the depth of the intended destination.
           FileUtils.mkdir_p(dd_dest(dest))
 
-          Jekyll.logger.debug(@tag_name, "Copying #{@name} to #{dd_dest(dest)}")
-          FileUtils.cp(path, File.join(dd_dest(dest), @name))
+          svg_dest_path = File.join(dd_dest(dest), @name)
+
+          if true  # TODO: Make this configurable
+            Jekyll.logger.debug(@tag_name, "Optimizing #{@name} and copying to #{dd_dest(dest)}")
+            # TODO: Make optimizations/plugins configurable
+            SvgOptimizer.optimize_file(path, svg_dest_path, SvgOptimizer::DEFAULT_PLUGINS)
+          else
+            Jekyll.logger.debug(@tag_name, "Copying #{@name} to #{dd_dest(dest)}")
+            FileUtils.cp(path, svg_dest_path)
+          end
 
           true
         end
