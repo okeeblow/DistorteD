04b15ca5af67cd0b1d8d9d9a449e0c4f1284c94a
Allison Reid <root@cooltrainer.org>
2020-07-08 11:14:15 -0700

    SVG Optimizer should not be used directly in Jekyll-land

--- DistorteD-Floor/lib/distorted-floor/media_molecule/svg.rb
+++ DistorteD-Floor/lib/distorted-floor/media_molecule/svg.rb
@@ -1,4 +1,7 @@
+require 'set'
+
 require 'mime/types'
+require 'svg_optimizer'
 
 module Cooltrainer
   class DistorteD
@@ -7,6 +10,11 @@ module Cooltrainer
       SUB_TYPE = 'svg'.freeze
 
       MIME_TYPES = MIME::Types[/^#{self::MEDIA_TYPE}\/#{self::SUB_TYPE}/, :complete => true].to_set
+
+      def self.optimize(src, dest)
+        # TODO: Make optimizations/plugins configurable
+        SvgOptimizer.optimize_file(src, dest, SvgOptimizer::DEFAULT_PLUGINS)
+      end
 
     end
   end
