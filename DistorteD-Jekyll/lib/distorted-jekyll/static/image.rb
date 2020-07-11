14a5c3f7a91a827dc528375467da05aba2eb5207
Allison Reid <root@cooltrainer.org>
2020-07-11 16:55:37 -0700

    Use an instance variable for our Vips::Image so subclasses can provide their own.

--- DistorteD-Jekyll/lib/distorted-jekyll/static/image.rb
+++ DistorteD-Jekyll/lib/distorted-jekyll/static/image.rb
@@ -27,10 +27,12 @@ module Jekyll
           # Create any directories to the depth of the intended destination.
           FileUtils.mkdir_p(dd_dest(dest))
 
-          distorted = DRIVER.new(path)
+          unless defined? @distorted
+            @distorted = DRIVER.new(path)
+          end
 
           Jekyll.logger.debug(@tag_name, "Rotating #{@name} if tagged.")
-          distorted.rotate(angle: :auto)
+          @distorted.rotate(angle: :auto)
 
           # Save every desired variation of this image.
           # This will be a Set of Hashes each describing the name, type,
@@ -40,7 +42,7 @@ module Jekyll
             if DRIVER::MIME_TYPES.include?(variation&.dig(:type))
               filename = File.join(dd_dest(dest), variation&.dig(:name) || @name)
               Jekyll.logger.debug('DistorteD Writing:', filename)
-              distorted.save(filename, width: variation&.dig(:width), crop: variation&.dig(:crop))
+              @distorted.save(filename, width: variation&.dig(:width), crop: variation&.dig(:crop))
             end
           end
 
