094cee315be422cf0ff3cedc649f80ba502b9668
Allison Reid <root@cooltrainer.org>
2020-07-16 14:35:32 -0700

    Follow the docs' advice on having a StandardError subclass

--- DistorteD-Jekyll/lib/distorted-jekyll/error_code.rb
+++ DistorteD-Jekyll/lib/distorted-jekyll/error_code.rb
@@ -1,14 +1,17 @@
+require 'distorted/error_code'
+
+
 module Jekyll
   module DistorteD
     # The built-in NotImplementedError is for "when a feature is not implemented
     # on the current platform", so make our own more appropriate ones.
-    class MediaTypeNotImplementedError < StandardError
+    class MediaTypeNotImplementedError < StandardDistorteDError
       attr_reader :media_type, :name
       def initialize(name)
         super("No supported media type for #{name}")
       end
     end
-    class MediaTypeNotFoundError < StandardError
+    class MediaTypeNotFoundError < StandardDistorteDError
       attr_reader :media_type, :name
       def initialize(name)
         super("Failed to detect media type for #{name}")
