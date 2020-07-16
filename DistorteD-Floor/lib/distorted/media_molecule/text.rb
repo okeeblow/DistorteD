bfdac3643d152d71552bba3f4339096ad024fbd2
Allison Reid <root@cooltrainer.org>
2020-07-15 23:43:44 -0700

    Add a Text driver for rendering plaintext and nfo to image.
    - Use Vips::Image.text
    - Implement g_markup_escape_text for Pango markup
    - Use the Perfect DOS 437 VGA Font
    - Monospace, justified, and zero-line-spacing only for now

---
+++ DistorteD-Floor/lib/distorted/media_molecule/text.rb
@@ -0,0 +1,133 @@
+require 'set'
+
+require 'distorted/monkey_business/string'
+
+require 'mime/types'
+
+module Cooltrainer
+  class DistorteD
+    class Text < Image
+
+      MEDIA_TYPE = 'text'.freeze
+
+      MIME_TYPES = MIME::Types[/^#{self::MEDIA_TYPE}\/(plain|x-nfo)/, :complete => true].to_set
+
+      ATTRS = Set[
+        :alt,
+        :font,
+        :encoding,
+      ]
+      ATTRS_VALUES = {
+        # Control font selection between the original "Perfect DOS VGA 437" font
+        # and the two "Less-" and "More-Perfect" variants:
+        # https://www.dafont.com/font-comment.php?file=perfect_dos_vga_437
+        # https://laemeur.sdf.org/fonts/
+        :font => Set[:less, :more, :perfect, :ansi],
+      }
+      ATTRS_DEFAULT = {
+        :font => :ansi,
+        :encoding => 'UTF-8'.freeze,
+      }
+
+      # Avoid renaming these from the original archives / websites.
+      FONT_FILENAME = {
+        :less => 'LessPerfectDOSVGA.ttf'.freeze,
+        :more => 'MorePerfectDOSVGA.ttf'.freeze,
+        :perfect => 'Perfect DOS VGA 437 Win.ttf'.freeze,
+        :ansi => 'Perfect DOS VGA 437.ttf'.freeze,
+      }
+
+
+      # Escape text as necessary for Pango Markup, which is what Vips::Image.text()
+      # expects for its argv. This code should be in GLib but is unimplemented in Ruby's:
+      #
+      # https://ruby-gnome2.osdn.jp/hiki.cgi?Gtk%3A%3ALabel#Markup+%28styled+text%29
+      # "The markup passed to Gtk::Label#set_markup() must be valid; for example,
+      # literal </>/& characters must be escaped as &lt;, &gt;, and &amp;.
+      # If you pass text obtained from the user, file, or a network to
+      # Gtk::Label#set_markup(), you'll want to escape it
+      # with GLib::Markup.escape_text?(not implemented yet)."
+      #
+      # Base my own implementation on the original C version found in gmarkup:
+      # https://gitlab.gnome.org/GNOME/glib/-/blob/master/glib/gmarkup.c
+      def g_markup_escape_text(text)
+        text.map{ |c| g_markup_escape_char(c) }
+      end
+
+      # The char-by-char actual function used by g_markup_escape_text
+      def g_markup_escape_char(c)
+        # I think a fully-working version of this function would
+        # be as simple `sprintf('&#x%x;', c.ord)`, but I want to copy
+        # the C implementation as closely as possible, which means using
+        # the named escape sequences for common characters and separating
+        # the Unicode control characters (> 0x7f) even though three's no
+        # need to in Ruby.
+        case c.ord
+        when '&'.ord
+          '&amp;'
+        when '<'.ord
+          '&lt;'
+        when '>'.ord
+          '&gt;'
+        when '\''.ord
+          '&apos;'
+        when '"'.ord
+          '&quot;'
+        when 0x1..0x8, 0xb..0xc, 0xe..0x1f, 0x7f
+          sprintf('&#x%x;', c.ord)
+        when 0x7f..0x84, 0x86..0x9f
+          sprintf('&#x%x;', c.ord)
+        else
+          c
+        end
+      end
+
+      # Return a Pango Markup escaped version of the document.
+      def to_pango
+        # https://developer.gnome.org/glib/stable/glib-Simple-XML-Subset-Parser.html#g-markup-escape-text
+        "<tt>" << @contents.encode('utf-8', :invalid => :replace, :replace => '').map{ |c|
+          g_markup_escape_char(c)
+        } << "</tt>"
+      end
+
+      def initialize(src, font: ATTRS_DEFAULT[:font], encoding: ATTRS_DEFAULT[:encoding])
+        @src = src
+        # VIPS makes us provide the text content as a single variable,
+        # so we may as well just one-shot File.read() it into memory.
+        @contents = File.read(src, **{:encoding => encoding.to_s})
+        @font = font
+
+        # https://libvips.github.io/libvips/API/current/libvips-create.html#vips-text
+        @image = Vips::Image.text(
+          # This string must be well-escaped Pango Markup:
+          # https://developer.gnome.org/pango/stable/pango-Markup.html
+          # However the official function for escaping text is
+          # not implemented in Ruby GLib, so we have to do it ourselves.
+          to_pango,
+          **{
+            # String absolute path to TTF
+            :fontfile => font_filename(@font),
+            # It's not enough to just specify the TTF path;
+            # we must also specify a font family, subfamily, and size.
+            :font => 'Perfect DOS VGA 437 Regular 16',#'Perfect DOS VGA 437 Win Regular 16',
+            # Zero space between lines (in Points)
+            :spacing => 0,
+            # Requires libvips 8.8; is there a way to detect this?
+            :justify => true,
+          },
+        )
+      end
+
+      # Return the String absolute path to the TTF file
+      def font_filename(font)
+        File.join(
+          File.dirname(__FILE__),
+          'font'.freeze,
+          'IBM-PC'.freeze,
+          FONT_FILENAME[font],
+        )
+      end
+
+    end  # Text
+  end  # DistorteD
+end  # Cooltrainer
---
+++ DistorteD-Jekyll/lib/distorted-jekyll/static/text.rb
@@ -0,0 +1,48 @@
+require 'set'
+
+require 'distorted/text'
+require 'distorted-jekyll/static/state'
+
+module Jekyll
+  module DistorteD
+    module Static
+      class Text < Image
+
+        DRIVER = Cooltrainer::DistorteD::Text
+
+        MEDIA_TYPE = DRIVER::MEDIA_TYPE
+        MIME_TYPES = DRIVER::MIME_TYPES
+
+        ATTRS = DRIVER::ATTRS
+        ATTRS_DEFAULT = DRIVER::ATTRS_DEFAULT
+        ATTRS_VALUES = DRIVER::ATTRS_VALUES
+
+
+        # dest: String realpath to `_site` directory
+        def write(dest)
+          orig_dest = destination(dest)
+
+          #return false if File.exist?(path) && !modified?
+          self.class.mtimes[path] = mtime
+
+          @distorted = DRIVER.new(path, encoding: attr_value(:encoding))
+          # Write any actual-text output variations.
+          # Images will be written by `super`.
+          for variation in files
+            if DRIVER::MIME_TYPES.include?(variation&.dig(:type))
+              filename = File.join(dd_dest(dest), variation&.dig(:name) || @name)
+              Jekyll.logger.debug('DistorteD Writing:', filename)
+              # TODO: For now this is just copying the file, but we should
+              # probably support some sort of UTF conversion or something here.
+              FileUtils.cp(path, filename)
+            end
+          end
+
+          super
+
+        end
+
+      end  # Text
+    end  # Static
+  end  # DistorteD
+end  # Jekyll
