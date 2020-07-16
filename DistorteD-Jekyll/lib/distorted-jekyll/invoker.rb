0c9af93c69865281fbb1942632a63fd7989ee46a
Allison Reid <root@cooltrainer.org>
2020-07-15 23:43:44 -0700

    Add a Text driver for rendering plaintext and nfo to image.
    - Use Vips::Image.text
    - Implement g_markup_escape_text for Pango markup
    - Use the Perfect DOS 437 VGA Font
    - Monospace, justified, and zero-line-spacing only for now

--- DistorteD-Jekyll/lib/distorted-jekyll/invoker.rb
+++ DistorteD-Jekyll/lib/distorted-jekyll/invoker.rb
@@ -9,6 +9,7 @@ require 'distorted-jekyll/molecule/abstract'
 
 # Media-type drivers
 require 'distorted-jekyll/molecule/image'
+require 'distorted-jekyll/molecule/text'
 require 'distorted-jekyll/molecule/pdf'
 require 'distorted-jekyll/molecule/svg'
 require 'distorted-jekyll/molecule/video'
@@ -48,6 +49,7 @@ module Jekyll
       # TODO: Make this configurable.
       MEDIA_MOLECULES = [
         Jekyll::DistorteD::Molecule::LastResort,
+        Jekyll::DistorteD::Molecule::Text,
         Jekyll::DistorteD::Molecule::PDF,
         Jekyll::DistorteD::Molecule::SVG,
         Jekyll::DistorteD::Molecule::Video,
---
+++ DistorteD-Jekyll/lib/distorted-jekyll/media_molecule/text.rb
@@ -0,0 +1,68 @@
+require 'set'
+
+require 'distorted/text'
+require 'distorted-jekyll/static/text'
+
+module Jekyll
+  module DistorteD
+    module Molecule
+      module Text
+
+        DRIVER = Cooltrainer::DistorteD::Text
+
+        MEDIA_TYPE = DRIVER::MEDIA_TYPE
+        MIME_TYPES = DRIVER::MIME_TYPES
+
+        ATTRS = DRIVER::ATTRS
+        ATTRS_DEFAULT = DRIVER::ATTRS_DEFAULT
+        ATTRS_VALUES = DRIVER::ATTRS_VALUES
+
+
+        def render_to_output_buffer(context, output)
+          super
+          begin
+            # Strip out all non-displayable media-types, e.g. the actual text/whatever.
+            filez = files.keep_if{|f| f.key?(:type) && f&.dig(:type)&.media_type == 'image'.freeze}.map{ |f|
+              f.transform_values(&:to_s).transform_keys(&:to_s)
+            }
+            output << parse_template.render({
+              'name' => @name,
+              'path' => @dd_dest,
+              'alt' => attr_value(:alt),
+              'title' => attr_value(:title),
+              'sources' => filez,
+              'fallback_img' => fallback_img,
+            })
+          rescue Liquid::SyntaxError => l
+            unless Jekyll.env == 'production'.freeze
+              output << parse_template(name: 'error_code'.freeze).render({
+                'message' => l.message,
+              })
+            end
+          end
+          output
+        end
+
+        # Return the filename of the most-compatible output image
+        # for use as the fallback <img> tag inside our <picture>.
+        def fallback_img
+          best_ver = nil
+          files.keep_if{|f| f.key?(:type) && f&.dig(:type)&.media_type == 'image'.freeze}.each{ |f|
+            # PNG > WebP
+            if f&.dig(:type)&.sub_type == 'png'.freeze || best_ver.nil?
+              best_ver = f
+            end
+          }
+          # Return the filename of the biggest matched variation,
+          # otherwise use the original filename.
+          best_ver&.dig(:name) || @name
+        end
+
+        def static_file(*args)
+          Jekyll::DistorteD::Static::Text.new(*args)
+        end
+
+      end  # Text
+    end  # Molecule
+  end  # DistorteD
+end  # Jekyll
