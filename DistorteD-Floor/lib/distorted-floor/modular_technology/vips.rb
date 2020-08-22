28f4352b4cf5734ac9875265aed9802bffd806ba
Allison Reid <root@cooltrainer.org>
2020-08-22 16:34:54 -0700

    Support runtime detection of MIME::Types supported by libvips instead of hardcoding a list of supported image types. This will increase portability to untested environments and obviate the need to push new releases to support new image formats via libvips.

--- DistorteD-Floor/lib/distorted-floor/modular_technology/vips.rb
+++ DistorteD-Floor/lib/distorted-floor/modular_technology/vips.rb
@@ -33,6 +33,9 @@ rescue LoadError => le
   raise $!, "#{help}\n#{$!}", $!.backtrace
 end
 
+
+require 'set'
+
 require 'distorted/checking_you_out'
 
 
@@ -40,5 +43,36 @@ module Cooltrainer; end
 module Cooltrainer::DistorteD; end
 module Cooltrainer::DistorteD::Technology; end
 module Cooltrainer::DistorteD::Technology::Vips
+
+
+  # Returns a Set of MIME::Types supported by libvips
+  def self.supported_types
+    # Vips allows us to query supported *SAVE* types by suffix.
+    # There's a simple relationship between filetype and extension since
+    # libvips uses the suffix to pick the Saver module.
+    #
+    # Loader modules, on the other hand, are picked by sniffing the
+    # first few bytes of the file, so a list of file extensions for
+    # supported loadable formats won't always be complete.
+    # For example, SVG and PDF are usually supported as loaders
+    # (via rsvg and PDFium/Poppler)
+    # https://github.com/libvips/ruby-vips/issues/186
+    #
+    # irb(main)> Vips.get_suffixes
+    # => [".csv", ".mat", ".v", ".vips", ".ppm", ".pgm", ".pbm", ".pfm",
+    #     ".hdr", ".dz", ".png", ".jpg", ".jpeg", ".jpe", ".webp", ".tif",
+    #     ".tiff", ".fits", ".fit", ".fts", ".gif", ".bmp"]
+
+    Vips.get_suffixes.map{ |t|
+      # A single call to this will return a Set for a String input
+      CHECKING::YOU::OUT(t)
+    }.reduce { |c,t|
+      # Flatten the Set-of-Sets-of-Types into a Set-of-Types
+      (c || Set[]).merge(t)
+    }.keep_if { |t|
+      # Filter out any Types that aren't images (e.g. CSV)
+      t.media_type == 'image'
+    }
+  end
 
 end
