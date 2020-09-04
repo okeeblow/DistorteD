9b7b58fd937d3e87b78d9b37a2b2745ba7b53499
Allison Reid <root@cooltrainer.org>
2020-09-03 23:45:38 -0700

    Refactor Image molecule to remove StaticFile!

--- DistorteD-Floor/lib/distorted/media_molecule/image.rb
+++ DistorteD-Floor/lib/distorted/media_molecule/image.rb
@@ -3,13 +3,12 @@ require 'set'
 
 require 'distorted/checking_you_out'
 require 'distorted/modular_technology/vips'
+require 'distorted/injection_of_love'
 
 
 module Cooltrainer
   module DistorteD
-    class Image
-
-      include Cooltrainer::DistorteD::Technology::Vips
+    module Image
 
 
       # Attributes for our <picture>/<img>.
@@ -29,44 +28,8 @@ module Cooltrainer
         :loading => Set[:eager, :lazy],
       }
 
-
-      def initialize(src)
-        @image = Vips::Image.new_from_file(src)
-        @src = src
-      end
-
-      def rotate(angle: nil)
-        if angle == :auto
-          @image = @image&.autorot
-        end
-      end
-
-      def clean
-        # Nuke the entire site from orbit. It's the only way to be sure.
-        @image.get_fields.grep(/exif-ifd/).each {|field| @image.remove field}
-      end
-
-      def save(dest, width: nil, crop: nil)
-        begin
-          if width.nil? or width == :full
-            return @image.write_to_file(dest)
-          elsif width.respond_to?(:to_i)
-            ver = @image.thumbnail_image(
-              width.to_i,
-              # Use `self` namespace for constants so subclasses can redefine
-              **{:crop => crop || self.singleton_class.const_get(:ATTRS_DEFAULT)[:crop]},
-            )
-            return ver.write_to_file(dest)
-          end
-        rescue Vips::Error => v
-          if v.message.include?('No known saver')
-            # TODO: Handle missing output formats. Replacements? Skip it? Die?
-            return nil
-          else
-            raise
-          end
-        end
-      end  # save
+      include Cooltrainer::DistorteD::Technology::Vips
+      include Cooltrainer::DistorteD::InjectionOfLove
 
     end  # Image
   end  # DistorteD
