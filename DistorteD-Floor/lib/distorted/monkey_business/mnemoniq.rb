2a221fffdb212fccca8558acc15988ffe278a5f7
Allison Reid <root@cooltrainer.org>
2020-12-16 00:22:22 -0800

    Remove this old MIME::Types monkey-patch since our local DB and its interface now handle this for us. This makes us a better citizen among other Gems :)

--- DistorteD-Floor/lib/distorted/monkey_business/mnemoniq.rb
+++
@@ -1,8 +0,0 @@
-require 'mime/types'
-
-# MIME::Types#preferred_extension returns @extensions.first unless
-# otherwise set. I don't like some of the defaults, so this file
-# changes them.
-# Normally I don't like to monkey patch just on import without calling
-# some method, but this is one time I explicitly want to do that.
-MIME::Types['image/jpeg'].last.preferred_extension = 'jpg'
