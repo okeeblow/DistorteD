ea8bc17d64b4770726d56ef85453db30d46898d9
Allison Reid <root@cooltrainer.org>
2020-12-19 20:21:04 -0800

    Refactor PDF MediaMolecules: - Convert old-style ATTRIBUTES to Compounds - Move PDF Open Params stuff to the Jekyll layer since we only want that when generating URLs for the embed. - Make sure the generic Image Molecule doesn't declare PDF load support.

--- DistorteD-Floor/lib/distorted/media_molecule/pdf.rb
+++ DistorteD-Floor/lib/distorted/media_molecule/pdf.rb
@@ -11,85 +11,12 @@ module Cooltrainer::DistorteD::Molecule; end
 module Cooltrainer::DistorteD::Molecule::PDF
 
 
-  LOWER_WORLD = CHECKING::YOU::IN("application/pdf").to_hash
-
-  # https://developer.mozilla.org/en-US/docs/Web/HTML/Element/object#Attributes
-  # https://www.adobe.com/content/dam/acom/en/devnet/acrobat/pdfs/pdf_open_parameters.pdf
-  PDF_OPEN_PARAMS = Array[
-    # Keep the PDF Open Params in the order they are defined
-    # in the Adobe documentation, since it says they should
-    # be specified in the URL in that same order.
-    # Ruby's Set doesn't guarantee order, so use a plain Array here.
-    :nameddest,
-    :page,
-    :comment,
-    :collab,
-    :zoom,
-    :view,
-    :viewrect,
-    :pagemode,
-    :scrollbar,
-    :search,
-    :toolbar,
-    :statusbar,
-    :messages,
-    :navpanes,
-    :highlight,
-    :fdf,
+  # https://hexapdf.gettalong.org/documentation/reference/api/HexaPDF/Document/index.html#method-c-new
+  # https://hexapdf.gettalong.org/documentation/reference/api/HexaPDF/index.html#DefaultDocumentConfiguration
+  # https://hexapdf.gettalong.org/documentation/reference/api/HexaPDF/Task/Optimize.html
+  LOWER_WORLD = Hash[
+    CHECKING::YOU::OUT['application/pdf'] => nil,
   ]
-  ATTRIBUTES = Set[
-    :alt,
-    :caption,
-    :height,  #<object> viewer container height.
-    :width,  # <object> viewer container width.
-  ].merge(PDF_OPEN_PARAMS)
-
-  # "You cannot use the reserved characters =, #, and &.
-  # There is no way to escape these special characters."
-  RESERVED_CHARACTERS_FRAGMENT = '[^=#&]+'.freeze
-
-  FLOAT_INT_FRAGMENT = '[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)'.freeze
-  ZERO_TO_ONE_HUNDRED = /^(([1-9]\d?|1\d{1})([.,]\d{0,1})?|100([.,]0{1})?)$/
-
-  ATTRIBUTES_DEFAULT = {
-    :height => '100%'.freeze,
-    :width => '100%'.freeze,
-    # BEGIN PDF Open Parameters
-    :page => 1,
-    :view => :Fit,
-    :pagemode => :none,
-    :scrollbar => 1,
-    :toolbar => 1,
-    :statusbar => 1,
-    :messages => 0,
-    :navpanes => 1,
-    # END PDF Open Parameters
-  }
-
-  # Adobe's PDF Open Parameters documentation sez:
-  # "Individual parameters, together with their values (separated by & or #),
-  # can be no greater then 32 characters in length."
-  # …but then goes on to show some examples (like `comment`)
-  # that are clearly longer than 32 characters.
-  # Dunno. I'll err on the side of giving you a footgun.
-  ATTRIBUTES_VALUES = {
-    :nameddest => /^#{RESERVED_CHARACTERS_FRAGMENT}$/,
-    :page => /\d/,
-    :comment => /^#{RESERVED_CHARACTERS_FRAGMENT}$/,
-    :collab => /^(DAVFDF|FSFDF|DB)@#{RESERVED_CHARACTERS_FRAGMENT}$/,
-    :zoom => /^#{FLOAT_INT_FRAGMENT}(,#{FLOAT_INT_FRAGMENT},#{FLOAT_INT_FRAGMENT})?$/,
-    :view => /^Fit(H|V|B|BH|BV(,#{FLOAT_INT_FRAGMENT})?)?$/,
-    :viewrect => /^#{FLOAT_INT_FRAGMENT},#{FLOAT_INT_FRAGMENT},#{FLOAT_INT_FRAGMENT},#{FLOAT_INT_FRAGMENT}$/,
-    :pagemode => Set[:none, :thumbs, :bookmarks],
-    :scrollbar => Cooltrainer::BOOLEAN_VALUES,
-    :search => /^#{RESERVED_CHARACTERS_FRAGMENT}(,\s#{RESERVED_CHARACTERS_FRAGMENT})*$/,
-    :toolbar => Cooltrainer::BOOLEAN_VALUES,
-    :statusbar => Cooltrainer::BOOLEAN_VALUES,
-    :messages => Cooltrainer::BOOLEAN_VALUES,
-    :navpanes => Cooltrainer::BOOLEAN_VALUES,
-    :fdf => /^#{RESERVED_CHARACTERS_FRAGMENT}$/,
-  }
-
 
   # TODO: Use MuPDF instead of libvips magick-based PDF loader.
 
