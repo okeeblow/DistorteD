606ea32ff7b7624b30c7bb9b46f402fa3c5a7d39
Allison Reid <root@cooltrainer.org>
2021-01-14 23:06:59 -0800

    Use a temp directory instead of one single temp file for the Tk GUI.

--- DistorteD-Booth/lib/distorted-booth/invoker.rb
+++ DistorteD-Booth/lib/distorted-booth/invoker.rb
@@ -1,6 +1,6 @@
 # https://saveriomiroddi.github.io/Installing-ruby-tk-bindings-gem-on-ubuntu/
 require 'tk'
-require 'tempfile'
+require 'tmpdir'
 
 require 'distorted/invoker'
 require 'distorted/checking_you_out'
@@ -29,26 +29,38 @@ class Cooltrainer::DistorteD::Booth
       raise ArgumentError.new('Please provide a media file to open.')
     end
 
-    temp = Tempfile.new([File.basename(@name, '.*'), '.ppm'])
+    # Tk's only built-in color image format.
     type = CHECKING::YOU::OUT['image/x-portable-pixmap']
+    change = Cooltrainer::Change.new(type, src: @name)
 
+    # Init DistorteD for our source file.
     tkd = TkDistorteD.new(File.expand_path(@name))
-    tkd.send(type.distorted_file_method, temp.path)
-    temp.close
 
-    root = TkRoot.new(
-      :title => "#{File.basename(@name)} (#{File.dirname(@name)}) — DistorteD",
-    )
+    # Create a temporary directory that will be removed
+    # after the execution of this block.
+    Dir.mktmpdir do |temp_dir|
+      # Write our PPM file to the temp directory
+      tkd.send(type.distorted_file_method, temp_dir, change, **{})
 
-    image = TkPhotoImage.new
-    image.file(temp.path)
+      # Create a root window
+      root = TkRoot.new(
+        :title => "#{File.basename(@name)} (#{File.dirname(@name)}) — DistorteD",
+      )
 
-    label = TkLabel.new(root) 
-    label.image = image
-    label.place(:height => image.height, :width => image.width)
+      # Create a container to display our PPM from the temp path
+      image = TkPhotoImage.new
+      image.file(change.paths(temp_dir).first)
 
-    root.configure(:width => image.width, :height => image.height)
-    Tk.mainloop
+      # The Image frame has to be in a Label sized to match.
+      label = TkLabel.new(root)
+      label.image = image
+      label.place(:height => image.height, :width => image.width)
+
+      # And the main window should be resized as well.
+      root.configure(:width => image.width, :height => image.height)
+      Tk.mainloop
+    end
+
   end
 
 end
