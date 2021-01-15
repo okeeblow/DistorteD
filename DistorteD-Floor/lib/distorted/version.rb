d83036934709ecdf80a22ad89b983c59af3797c3
Allison Reid <root@cooltrainer.org>
2021-01-15 00:50:59 -0800

    Version 0.7️!!

--- DistorteD-Floor/lib/distorted/version.rb
+++ DistorteD-Floor/lib/distorted/version.rb
@@ -1,22 +1,22 @@
-#                                                                                                                        
-#            `.........-`                                                                      `:/:::://.`               
-#          `+/``+ssss:``-/:`                                                                 `:o+y. `::::-----``         
-#         -+- :hmNNmdhs- `o++`             `-:--/                            -/...         ./shhyoy. +mmmmds+:--...`     
-#       .//``odmNmmd+++` :+/h:-------------:+. ./...-..........---/--:.------//  -.--------//o//o+/+- :hhdmmmmmh/`-/-`   
-#     `:+- :ymNmmyy:...-:` /: ./////////////-  ::/:/- `++++++++` -y  /:.`.////+-  /////- `/+++++++. :- .::+sydmNm+ `/+.  
-#    -+:` /syyyo:--:+oso` /:  ---:::::::syys. -yoy+s` :mmmmmmmd- .h` ``:osshhyyy` /oshhs` -:::::::.  :: `-..-/dmo` `/yo: 
-#  `/o:--------:+syyy++``+s+++++++++/``:h+os``oshdos``odhhhddyy-`.h.`.ssyhmmdmoh/``o+mmho``:++++++++++y+``----::``-oyss. 
-#  +sssssssyyyyyhdmmsyo-+/----------.`.yooy-`-hodss/``---------.`.h-`.sohdhsymhsy.`:+yNmy/`.-----------:++ooooooo+sss+`  
-#  `+dmmmdddddmmNNddhsyosooooooooooooooysoysssyymssssssssssssssssshyssyoh:` .mhsyssssodddyooooooooooooooysoyyyyyyyyy/    
-#    /mNmdddhmdmmdy+-+mdhhhhhhhhhhhhhhhmhhyhhhmdmmdhhhhhhhhhhhhhhhdhhhsm+    ydddhhhhd-hdmdddddddddddddddyhdhdmmNms.     
-#     .---.......`    -yhNmmhddhhhdhmhmhddmNNmyyy+dmNdydhyyyhddhmmmmNmhh.    -hyhNNNh: .mNmdmdddhhhdddmmyssssssso:       
-#                       `/oooooooooooooo:`-+o+/.  `+oo+o++++ooo+oooooo-       `-+oo/`   :oooooooooooooo/                 
-#                                                                                                                        
+#                                                                                                                       
+#            `.........-`                                                                      `:/:::://.`              
+#          `+/``+ssss:``-/:`                                                                 `:o+y. `::::-----``        
+#         -+- :hmNNmdhs- `o++`             `-:--/                            -/...         ./shhyoy. +mmmmds+:--...`    
+#       .//``odmNmmd+++` :+/h:-------------:+. ./...-..........---/--:.------//  -.--------//o//o+/+- :hhdmmmmmh/`-/-`  
+#     `:+- :ymNmmyy:...-:` /: ./////////////-  ::/:/- `++++++++` -y  /:.`.////+-  /////- `/+++++++. :- .::+sydmNm+ `/+. 
+#    -+:` /syyyo:--:+oso` /:  ---:::::::syys. -yoy+s` :mmmmmmmd- .h` ``:osshhyyy` /oshhs` -:::::::.  :: `-..-/dmo` `/yo:
+#  `/o:--------:+syyy++``+s+++++++++/``:h+os``oshdos``odhhhddyy-`.h.`.ssyhmmdmoh/``o+mmho``:++++++++++y+``----::``-oyss.
+#  +sssssssyyyyyhdmmsyo-+/----------.`.yooy-`-hodss/``---------.`.h-`.sohdhsymhsy.`:+yNmy/`.-----------:++ooooooo+sss+` 
+#  `+dmmmdddddmmNNddhsyosooooooooooooooysoysssyymssssssssssssssssshyssyoh:` .mhsyssssodddyooooooooooooooysoyyyyyyyyy/   
+#    /mNmdddhmdmmdy+-+mdhhhhhhhhhhhhhhhmhhyhhhmdmmdhhhhhhhhhhhhhhhdhhhsm+    ydddhhhhd-hdmdddddddddddddddyhdhdmmNms.    
+#     .---.......`    -yhNmmhddhhhdhmhmhddmNNmyyy+dmNdydhyyyhddhmmmmNmhh.    -hyhNNNh: .mNmdmdddhhhdddmmyssssssso:      
+#                       `/oooooooooooooo:`-+o+/.  `+oo+o++++ooo+oooooo-       `-+oo/`   :oooooooooooooo/                
+#                                                                                                                       
 
 require_relative 'triple_counter'
 
 module Cooltrainer
   module DistorteD
-    VERSION = TripleCounter.new(0, 6, 0)
+    VERSION = TripleCounter.new(0, 7, 0)
   end
 end
