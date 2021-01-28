2fcfb4c4bd8dcb279de4369f589b0859c1f2012d
Allison Reid <root@cooltrainer.org>
2021-01-27 22:09:45 -0800

    Support fallback Image Types!
    Use PNG if we have an alpha-channel, GIF if no alpha-channel and fewer than 256 colors, and JPEG otherwise.
    
    Add methods to modify a Change in-flight to add the Tag and new Type.

--- DistorteD-Jekyll/lib/distorted-jekyll/liquid_liquid/picture.rb
+++ DistorteD-Jekyll/lib/distorted-jekyll/liquid_liquid/picture.rb
@@ -8,6 +8,28 @@ module Jekyll::DistorteD::LiquidLiquid; end
 module Jekyll::DistorteD::LiquidLiquid::Picture
 
 
+  # Our <picture> tag needs a child <img> tag to display an appropriate <source> into.
+  # That <img> can be totally blank — <img/> — but we should fill it with a fallback
+  # for browsers that don't support <picture> that will fall through to just rendering
+  # the <img> and its `src`.
+  # Browsers old enough to lack <picture> support almost certainly lack support
+  # for newer Image Types like WebP/AVIF. Use a MetaType to trigger the selection
+  # and rendering of one (1) fallback.
+  FALLBACK_IMAGE_TYPE = CHECKING::YOU::OUT['image/x.distorted.fallback']
+  OUTER_LIMITS = Hash[FALLBACK_IMAGE_TYPE => nil]
+
+  # Given an input Change, what MIME::Type should we use as the fallback?
+  def detect_fallback_image_type
+    @fallback_image_type ||= case
+    when to_vips_image.has_alpha? then CHECKING::YOU::OUT['image/png'.freeze]
+    when to_vips_image.percent(100) <= 256 then CHECKING::YOU::OUT['image/gif'.freeze]
+    else CHECKING::YOU::OUT['image/jpeg'.freeze]
+    end
+  end
+
+  # Returns a modified copy of a Change that includes the new fallback Type and Tag
+  def change_fallback_image(change)
+    change_fallback_image!(change.dup)
   end
 
   # Returns a Change modified in-place to includ the new fallback Type and Tag
