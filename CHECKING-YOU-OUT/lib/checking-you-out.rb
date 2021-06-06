46dc79e485417b6c8e16b96e2b13ceba461a098e
Allison Reid <root@cooltrainer.org>
2021-06-06 00:42:16 -0700

    Only try to parse an unknown identifier as an IETF String if it has a single `/` character, and prepare to handle the case where we don't know what to do with an unknown identifier.

--- CHECKING-YOU-OUT/lib/checking-you-out.rb
+++ CHECKING-YOU-OUT/lib/checking-you-out.rb
@@ -10,12 +10,15 @@ class CHECKING::YOU
       ::CHECKING::YOU::OUT::from_pathname(unknown_identifier)
     when ::String
       case
-      when unknown_identifier.start_with?('.'.freeze) && unknown_identifier.count('.'.freeze) == 1
+      when unknown_identifier.start_with?(-?.) && unknown_identifier.count(-?.) == 1
         ::CHECKING::YOU::OUT::from_postfix(unknown_identifier)
       when File.exist?(unknown_identifier)
         ::CHECKING::YOU::OUT::from_pathname(Pathname.new(unknown_identifier))
-      when unknown_identifier.count('/'.freeze)
+      when unknown_identifier.count(-?/) == 1
         ::CHECKING::YOU::OUT::from_ietf_media_type(unknown_identifier)
+      else
+        # TODO: Raise something here? Return an empty (but allocated) Set?
+        nil
       end
       # TODO: A String arg could also be a path, an extname with no leading dot,
       # or an entire file stream.
