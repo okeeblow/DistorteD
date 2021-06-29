2e1b82aed1aec771a606150fbbcad971e7f7fda9
Allison Reid <root@cooltrainer.org>
2021-06-29 00:11:36 -0700

    Use Weighted Globs to separate file extensions from all other pathname matches.

--- CHECKING-YOU-OUT/lib/checking-you-out/ghost_revival/mr_mime.rb
+++ CHECKING-YOU-OUT/lib/checking-you-out/ghost_revival/mr_mime.rb
@@ -188,6 +188,7 @@ class CHECKING::YOU::MrMIME < ::Ox::Sax
       @weighted_action = ::CHECKING::YOU::OUT::CatSequence.new
     when :"magic-deleteall"
       # TODO
+    when :glob
     when :"glob-deleteall"
       # TODO
     when :treemagic
@@ -222,9 +223,32 @@ class CHECKING::YOU::MrMIME < ::Ox::Sax
       self.cyo.add_aka(::CHECKING::YOU::IN::from_ietf_media_type(value.as_s))
     in :"sub-class-of", :type
       self.cyo.add_parent(::CHECKING::YOU::OUT::from_ietf_media_type(value.as_s))
+    in :glob, :weight
+      if @stick_around.nil?
+        @stick_around = value.as_i
+      else
+        @stick_around.weight = value.as_i
+      end
     in :glob, :pattern
-      # TODO: Make this less fragile. It assumes all`<glob>` patterns are of the form `*.ext` (they are)
-      self.cyo.add_postfix(value.as_s.delete_prefix!(-'*.')||value.as_s)
+      # The vast majority of `<glob>`s in `shared-mime-info`'s XML will represent file-extensions, e.g. `*.lol`,
+      # but there are some prefixes represented, e.g. `Makefile*`, as well as the capability for arbitrary globs.
+      # We will decompose these to CYO's `Postfix`, `Prefix`, and `Infix` to allow file extensions to be stored separately.
+      @stick_around = value.as_s.yield_self { |strval|
+        # NOTE: As I write this in 2021 there are no `<glob>` patterns in fd.o with more than one asterisk,
+        # so I feel safe using `:delete_prefix`/`:delete_suffix` here.
+        # The weight IVar here will either be Integer or nil, both of which are handled.
+        case
+        when strval.delete_prefix!(-'*.') then
+          # e.g. "*.jpeg" => -"jpeg"
+          ::CHECKING::YOU::OUT::Postfix.new(str=-strval, weight: @stick_around)
+        else
+          # There are some exact patterns, e.g. "winmail.dat".
+          # There are Regexp-style patterns like "[0-9][0-9][0-9].vdr" for `video/mpeg`.
+          # There are prefix-style patterns like "Makefile.*"
+          # Instead of converting to Regexp we can use `File::fnmatch`.
+          ::CHECKING::YOU::OUT::Glob.new(str=-strval, weight: @stick_around)
+        end
+      }
     in :"root-XML", :namespaceURI
       # TODO
     in :"root-XML", :localName
@@ -260,6 +284,8 @@ class CHECKING::YOU::MrMIME < ::Ox::Sax
       @weighted_action.pop
     when :magic
       @weighted_action.clear
+    when :glob
+      self.cyo.add_pathname_fragment(@stick_around) unless @stick_around.nil?
     end
   end
 
