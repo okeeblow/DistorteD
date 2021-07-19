a3651723ee4837a66d1f7e9dbc4d00530aaaccc6
Allison Reid <root@cooltrainer.org>
2021-07-18 18:21:43 -0700

    Add `Hash`-key tests for `StickAround` and a couple extra tests for mixed-case `String`s.

--- CHECKING-YOU-OUT/TEST-MY-BEST/tmb_party_starter/tmb_stick_around.rb
+++ CHECKING-YOU-OUT/TEST-MY-BEST/tmb_party_starter/tmb_stick_around.rb
@@ -19,5 +19,20 @@ class TestStickAround < Test::Unit::TestCase
     assert_equal(@stick_around_insensitive_down, 'DOC')
     assert_equal(@stick_around_sensitive_down, 'doc')
     assert_not_equal(@stick_around_sensitive_down, 'DOC')
+    assert_not_equal(@stick_around_sensitive_up, 'doc')
+    assert_not_equal(@stick_around_sensitive_down, 'DoC')
+    assert_not_equal(@stick_around_sensitive_up, 'DoC')
+  end
+
+  def test_rb_str_hash_cmp
+    @hash[@stick_around_insensitive_down] = :hey
+    @hash[@stick_around_sensitive_up] = :sup
+    assert_equal(@hash[@stick_around_sensitive_down], :hey)
+    assert_equal(@hash[@stick_around_insensitive_down], :hey)
+    assert_equal(@hash[@stick_around_sensitive_up], :sup)
+    assert_equal(@hash[@stick_around_insensitive_up], :sup)
+    assert_not_equal(@hash[@stick_around_sensitive_down], :sup)
+    assert_not_equal(@hash[@stick_around_sensitive_up], :hey)
+    @hash.clear
   end
 end
