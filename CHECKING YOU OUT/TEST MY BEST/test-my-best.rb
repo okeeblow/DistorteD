3d4b8cd81dadfc5b37628e8a009aa4b0d94936b6
Allison Reid <root@cooltrainer.org>
2021-07-18 17:40:03 -0700

    Add unit-testing loader and entry-script.

---
+++ CHECKING YOU OUT/TEST MY BEST/test-my-best.rb
@@ -0,0 +1,14 @@
+# This is how we're testing.
+require 'test/unit'
+
+# This is what we're testing.
+require_relative '../lib/checking-you-out'
+
+# Use `Kernel#load` for discovered tests instead of `Kernel#require` or `#require_relative`,
+# because `#load` supports paths relative to CWD in addition to just to `$LOAD_PATH` (like `#require`)
+# or to `__file__` (like `#require_relative`).
+# This lets me have a nice easy entry-point in `GEM_ROOT/bin` instead of having to `cd` in first.
+#
+# These files should contain a subclass of `Test::Unit::TestCase` with methods named like `test_whatever`
+# which `test/unit` will automatically discover and execute as long as they're loaded.
+Dir.glob("**/tmb_*.rb").each { load _1 }
