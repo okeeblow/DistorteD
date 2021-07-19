c3aadb6a68cf4b88fa857d805904327ef09e37b3
Allison Reid <root@cooltrainer.org>
2021-07-18 20:43:20 -0700

    Handle case-sensitivity properly when setting `StickAround`'s value in `:initialize`, using `#replace`'s logic. Refactor `#replace`'s logic to be less confusing, and add relevant tests.

--- CHECKING-YOU-OUT/TEST-MY-BEST/tmb_party_starter/tmb_stick_around.rb
+++ CHECKING-YOU-OUT/TEST-MY-BEST/tmb_party_starter/tmb_stick_around.rb
@@ -4,9 +4,9 @@ class TestStickAround < Test::Unit::TestCase
     @stick_around_insensitive_up = ::CHECKING::YOU::StickAround.new('DOC')
     @stick_around_sensitive_down = ::CHECKING::YOU::StickAround.new('doc', case_sensitive: true)
     @stick_around_sensitive_up = ::CHECKING::YOU::StickAround.new('DOC', case_sensitive: true)
-    @hash = Hash.new
   end
 
+  # `StickAround` vs. `StickAround`
   def test_stick_around_case_insensitive_sensitive_equal
     assert_equal(@stick_around_insensitive_down, @stick_around_insensitive_up)
     assert_equal(@stick_around_insensitive_down, @stick_around_sensitive_down)
@@ -14,6 +14,7 @@ class TestStickAround < Test::Unit::TestCase
     assert_not_equal(@stick_around_insensitive_down, @stick_around_sensitive_up)
   end
 
+  # `StickAround` vs. `String`
   def test_stick_around_string_equal
     assert_equal(@stick_around_insensitive_down, 'doc')
     assert_equal(@stick_around_insensitive_down, 'DOC')
@@ -24,15 +25,28 @@ class TestStickAround < Test::Unit::TestCase
     assert_not_equal(@stick_around_sensitive_up, 'DoC')
   end
 
+  # `StickAround` is a `String` subclass and so can't escape MRI's C implementation
+  # of `String`-as-`Hash`-key comparison code (`rb_str_hash_cmp`).
   def test_rb_str_hash_cmp
-    @hash[@stick_around_insensitive_down] = :hey
-    @hash[@stick_around_sensitive_up] = :sup
-    assert_equal(@hash[@stick_around_sensitive_down], :hey)
-    assert_equal(@hash[@stick_around_insensitive_down], :hey)
-    assert_equal(@hash[@stick_around_sensitive_up], :sup)
-    assert_equal(@hash[@stick_around_insensitive_up], :sup)
-    assert_not_equal(@hash[@stick_around_sensitive_down], :sup)
-    assert_not_equal(@hash[@stick_around_sensitive_up], :hey)
-    @hash.clear
+    hash = Hash.new
+    hash[@stick_around_insensitive_down] = :hey
+    hash[@stick_around_sensitive_up] = :sup
+
+    # `StickAround` vs `StickAround`
+    assert_equal(hash[@stick_around_sensitive_down], :hey)
+    assert_equal(hash[@stick_around_insensitive_down], :hey)
+    assert_equal(hash[@stick_around_sensitive_up], :sup)
+    assert_equal(hash[@stick_around_insensitive_up], :hey)  # Nondeterministic depending on insert order. We inserted insensitive-lower first.
+    assert_not_equal(hash[@stick_around_sensitive_down], :sup)
+    assert_not_equal(hash[@stick_around_sensitive_up], :hey)
+
+    # `StickAround` vs `String`
+    assert_equal(hash['doc'], :hey)
+
+    hash.clear
+
+    # This same C method also applies to `Set` and `#uniq`.
+    assert_equal(Set[@stick_around_insensitive_up, @stick_around_insensitive_down].size, 1)
+    assert_equal(Array[@stick_around_insensitive_up, @stick_around_insensitive_down].uniq!.size, 1)
   end
 end
