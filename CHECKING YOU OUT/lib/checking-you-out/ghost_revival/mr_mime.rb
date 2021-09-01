3bfa4f8bd28ce077881bfc5c4b2732e8e0610b67
Allison Reid <root@cooltrainer.org>
2021-08-31 19:12:50 -0700

    Move system endianness detection to xross-platform module.

--- CHECKING YOU OUT/lib/checking-you-out/ghost_revival/mr_mime.rb
+++ CHECKING YOU OUT/lib/checking-you-out/ghost_revival/mr_mime.rb
@@ -28,28 +28,6 @@ require 'ox'
 class CHECKING::YOU::MrMIME < ::Ox::Sax
 
 
-  ORIGIN_OF_SYMMETRY = proc {
-    # Little-endian systems:
-    # - VAX
-    # - x86 / AMD64
-    # Big-endian systems:
-    # - Motorola 68k
-    # - Internet https://en.wikipedia.org/wiki/Endianness#Networking
-    # - IBM mainframes
-    # Bi-endian systems:
-    # - AArch64
-    # - PowerPC / POWER
-    # - MIPS
-    # - Alpha
-    # - PA-RISC
-    # - SuperH
-    # - Itanium
-    # - RISC-V
-    @host_endianness ||= [1].yield_self { |bliss|
-      # Pack the test Integer as a native-endianness 'I'nt and a 'N'etwork-endianess (BE) Int and compare.
-      bliss.pack(-?I) == bliss.pack(-?N) ? :BE : :LE
-    }
-  }
 
   # Turn an arbitrary String into the correctly-based Integer it represents.
   # It would be nice if I could do this directly in `Ox::Sax::Value`.
