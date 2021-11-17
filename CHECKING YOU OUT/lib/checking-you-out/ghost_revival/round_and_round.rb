5c063af078a2557517c6fb687d8cc936b0449afb
Allison Reid <root@cooltrainer.org>
2021-11-16 23:30:00 -0800

    Don't `move` coerced query objects between `Ractor`s.

--- CHECKING YOU OUT/lib/checking-you-out/ghost_revival/round_and_round.rb
+++ CHECKING YOU OUT/lib/checking-you-out/ghost_revival/round_and_round.rb
@@ -2,6 +2,26 @@ module ::CHECKING::YOU::OUT::GHOST_REVIVAL::ROUND_AND_ROUND
   def send(query, area_code: ::CHECKING::YOU::IN::DEFAULT_AREA_CODE)
     ::CHECKING::YOU::OUT::GHOST_REVIVAL::AREAS[area_code].send(query)
   end
+
+  # Control `Ractor#send(move: true/false)` based on whether or not we are coercing the given input to a new `Class`.
+  # This is to work around the inability for `Ractor::make_shareable` to `#freeze` the `IO` stream in a `Wild_I/O`,
+  # avoiding a `Ractor::MovedError` if we tried to use the same query `Object` twice in a row.
+  #
+  # Compare explicit `move: true`:
+  #   irb> lumix = Pathname::new('/home/okeeblow/Works/DistorteD/CHECKING YOU OUT/TEST MY BEST/Try 2 Luv. U/x-content/image-dcf/LUMIX')
+  #   irb> CYO::from_pathname(lumix).description => "digital photos"
+  #   irb> CYO::from_pathname(lumix).description
+  #   /home/okeeblow/Works/DistorteD/CHECKING YOU OUT/lib/checking-you-out/ghost_revival/round_and_round.rb:11:in `p':
+  #     undefined method `inspect' for #<Ractor::MovedObject:0x0000564fbcdd2040> (NoMethodError)
+  #         from /home/okeeblow/Works/DistorteD/CHECKING YOU OUT/lib/checking-you-out/ghost_revival/round_and_round.rb:11:in `[]'
+  #         from /home/okeeblow/Works/DistorteD/CHECKING YOU OUT/lib/checking-you-out/ghost_revival/round_and_round.rb:37:in `from_pathname'
+  #         from (irb):3:in `<main>'
+  #         from ./bin/repl:11:in `<main>'
+  #
+  # …to `move: coerce.nil?`:
+  #   irb> lumix = Pathname::new('/home/okeeblow/Works/DistorteD/CHECKING YOU OUT/TEST MY BEST/Try 2 Luv. U/x-content/image-dcf/LUMIX')
+  #   irb> CYO::from_pathname(lumix).description => "digital photos"
+  #   irb> CYO::from_pathname(lumix).description => "digital photos"
   def [](
     query,
     area_code: ::CHECKING::YOU::IN::DEFAULT_AREA_CODE,
@@ -14,7 +34,7 @@ module ::CHECKING::YOU::OUT::GHOST_REVIVAL::ROUND_AND_ROUND
       receiver
     )
     wanted = message.erosion_mark
-    ::CHECKING::YOU::OUT::GHOST_REVIVAL::AREAS.call(area_code).send(message, move: true)
+    ::CHECKING::YOU::OUT::GHOST_REVIVAL::AREAS.call(area_code).send(message, move: coerce.nil?)
     ::Ractor::receive_if {
       _1.is_a?(::CHECKING::YOU::IN::EverlastingMessage) and _1.erosion_mark == wanted
     }.in_motion if receiver == ::Ractor::current
