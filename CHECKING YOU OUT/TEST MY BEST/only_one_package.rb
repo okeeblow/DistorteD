ad0119392da54e34f86c5ab44cf2a3596a4c0d96
Allison Reid <root@cooltrainer.org>
2021-11-28 02:28:00 -0800

    Enable Tika package for unit tests.

--- CHECKING YOU OUT/TEST MY BEST/only_one_package.rb
+++ CHECKING YOU OUT/TEST MY BEST/only_one_package.rb
@@ -14,6 +14,18 @@ module ::CHECKING::YOU::OUT::OnlyOnePackage
       )
     )
   )
+  TIKA_MIME = ::Ractor::make_shareable(
+    ::CHECKING::YOU::OUT::GHOST_REVIVAL::DISCOVER_THE_LIFE::SharedMIMEinfo.new(
+      ::File.join(
+        ::CHECKING::YOU::OUT::GEM_ROOT.call,
+        -'mime',
+        -'packages',
+        -'third-party',
+        -'tika-mimetypes',
+        -'tika-mimetypes.xml',
+      )
+    )
+  )
   CYO_MIME = ::Ractor::make_shareable(proc {
     cyo_base = ::CHECKING::YOU::OUT::GEM_ROOT.call
     ::Dir::glob(
@@ -29,6 +41,6 @@ module ::CHECKING::YOU::OUT::OnlyOnePackage
   # TODO: Figure out why Refining our module (or module.singleton_class) doesn't work here.
   #refine ::CHECKING::YOU::OUT::GHOST_REVIVAL::DISCOVER_THE_LIFE.singleton_class do
   module ::CHECKING::YOU::OUT::GHOST_REVIVAL::DISCOVER_THE_LIFE
-    def shared_mime_info_packages; ::Array.new.push(FDO_MIME).push(*CYO_MIME.call); end
+    def shared_mime_info_packages; ::Array.new.push(FDO_MIME).push(TIKA_MIME).push(*CYO_MIME.call); end
   end
 end
