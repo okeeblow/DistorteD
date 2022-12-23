6e0b01c77739b8d0db7c84385bf99085f4c9d55e
Allison Reid <root@cooltrainer.org>
2022-12-23 13:51:00 -0800

    Support setting `version=`, `variant=`, and combined `clock_seq=`

--- Globe Glitter/lib/globeglitter/inner_spirit.rb
+++ Globe Glitter/lib/globeglitter/inner_spirit.rb
@@ -13,9 +13,6 @@ module ::GlobeGlitter::INNER_SPIRIT
   def clock_seq_low               =  (self[:inner_spirit] >> 48) & 0xFF
   def node                        =   self[:inner_spirit]        & 0xFFFFFFFFFFFF
 
-  # Getter for Base-16 `::String` output where these two fields are combined.
-  def clock_seq = (self.clock_seq_high_and_reserved << 8) | self.clock_seq_low
-
   # Setters for fields defined in the specification.
   def time_low=(otra)
     self[:inner_spirit] = ((otra << 96) | (self[:inner_spirit] & 0xFFFFFFFF_FFFFFFFF_FFFFFFFF))
@@ -25,25 +22,33 @@ module ::GlobeGlitter::INNER_SPIRIT
       (self[:inner_spirit] & 0xFFFFFFFF_0000FFFF_FFFFFFFF_FFFFFFFF) | otra
     )
   end
-  def time_high_and_version=(otra)
+  def time_high=(otra)
     self[:inner_spirit] = (
-      (self[:inner_spirit] & 0xFFFFFFFF_FFFF0000_FFFFFFFF_FFFFFFFF) | ((otra & 0xF000) | (self.version << 12))
+      (self[:inner_spirit] & 0xFFFFFFFF_FFFFF000_FFFFFFFF_FFFFFFFF) | (otra & 0x0FFF)
+    )
+  end
+  def time=(otra)
+    self[:inner_spirit] = (
+      (self[:inner_spirit] & 0x00000000_0000F000_FFFFFFFF_FFFFFFFF) | ((otra & 0xFFFFFFFF_FFFF0FFF) << 64)
     )
   end
   def clock_seq_high_and_reserved=(otra)
     self[:inner_spirit] = (
       (self[:inner_spirit] & 0xFFFFFFFF_FFFFFFFF_00FFFFFF_FFFFFFFF) |
       (
-        (otra & case self.variant
-          when 0    then 0b01111111
-          when 1    then 0b00111111
-          when 2, 3 then 0b00011111
-        end) | (case self.variant
-          when 0    then 0b00000000
-          when 1    then 0b10000000
-          when 2    then 0b11000000
-          when 3    then 0b11100000
-        end)
+        (
+          (otra & case self.variant
+            when 0    then 0b01111111
+            when 1    then 0b00111111
+            when 2, 3 then 0b00011111
+          end) |
+          (case self.variant
+            when 0    then 0b00000000
+            when 1    then 0b10000000
+            when 2    then 0b11000000
+            when 3    then 0b11100000
+          end)
+        ) << 56
       )
     )
   end
@@ -55,6 +60,22 @@ module ::GlobeGlitter::INNER_SPIRIT
   def node=(otra)
     self[:inner_spirit] = (
       (self[:inner_spirit] & 0xFFFFFFFF_FFFFFFFF_FFFF0000_00000000) | otra
+    )
+  end
+
+  # Getter for Base-16 `::String` output where these two fields are combined.
+  def clock_seq = (self.clock_seq_high_and_reserved << 8) | self.clock_seq_low
+  def clock_seq=(otra)
+    self[:inner_spirit] = (
+      (self[:inner_spirit] & 0xFFFFFFFF_FFFFFFFF_0000FFFF_FFFFFFFF) |
+      (
+        case self.variant
+          when 0    then 0b00000000
+          when 1    then 0b10000000
+          when 2    then 0b11000000
+          when 3    then 0b11100000
+        end << 56
+      ) | (otra << 48)
     )
   end
 
@@ -86,7 +107,13 @@ module ::GlobeGlitter::INNER_SPIRIT
   #                                   that uses SHA-1 hashing.
   #
   # “The version is more accurately a sub-type; again, we retain the term for compatibility.”
-  def version = (self.time_high_and_version & 0xF000) >> 12
+  def version = (self[:inner_spirit] >> 76) & 0xF
+  def version=(otra)
+    raise ::ArgumentError::new("invalid version #{otra.to_s}") unless otra.is_a?(::Integer) and otra.between?(1, 8)
+    self[:inner_spirit] = (
+      (self[:inner_spirit] & 0xFFFFFFFF_FFFF0FFF_FFFFFFFF_FFFFFFFF) | (otra << 76)
+    )
+  end
 
   # ITU-T Rec. X.667 sez —
   #
@@ -108,6 +135,12 @@ module ::GlobeGlitter::INNER_SPIRIT
   #    1     1     0    Reserved, Microsoft Corporation backward compatibility
   #
   #    1     1     1    Reserved for future definition.
+  #
+  #
+  # NOTE: Some libraries (like `java.util.UUID`) specify the variant value as if it were not backwards-masked:
+  #       https://docs.oracle.com/en/java/javase/19/docs/api/java.base/java/util/UUID.html#variant()
+  #
+  #       I think it makes more sense for it to count upward like `version` rather than use the raw bit value.
   def variant
     # Can't use getter for this since the getter return value will rely on this variant.
     clock_seq_high_and_reserved = ((self[:inner_spirit] >> 56) & 0xFF)
@@ -119,6 +152,29 @@ module ::GlobeGlitter::INNER_SPIRIT
     when (clock_seq_high_and_reserved >> 5).eql?(0b110) then 2
     when (clock_seq_high_and_reserved >> 5).eql?(0b111) then 3
     end
+  end
+
+  def variant=(otra)
+    self[:inner_spirit] = (
+      (self[:inner_spirit] & 0xFFFFFFFF_FFFFFFFF_00FFFFFF_FFFFFFFF) |
+      (
+        (
+          (case otra 
+            when 0    then 0b00000000
+            when 1    then 0b10000000
+            when 2    then 0b11000000
+            when 3    then 0b11100000
+            else      raise ::ArgumentError::new("invalid variant #{otra.to_s}")
+          end) |
+          (self.clock_seq_high_and_reserved & case otra
+            when 0    then 0b01111111
+            when 1    then 0b00111111
+            when 2, 3 then 0b00011111
+            else      raise ::ArgumentError::new("invalid variant #{otra.to_s}")
+            end)
+        ) << 56
+      )
+    )
   end
 
 end
