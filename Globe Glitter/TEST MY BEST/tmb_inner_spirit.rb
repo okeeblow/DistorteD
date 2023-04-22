92c4765de4b88f9b174e72c4c080c8d617a95e42
Allison Reid <root@cooltrainer.org>
2023-04-21 19:56:22 -0700

    Re-enable structure/law tests with non-assignment methods

--- Globe Glitter/TEST MY BEST/tmb_inner_spirit.rb
+++ Globe Glitter/TEST MY BEST/tmb_inner_spirit.rb
@@ -182,20 +182,20 @@ class TestGlobeGlitterInnerSpirit < Test::Unit::TestCase
   def test_rules
     ::GlobeGlitter::nil.tap { |gg|
       assert_equal(gg.rules, ::GlobeGlitter::RULES_UNSET)
-      #assert_equal(::GlobeGlitter::RULES_TIME_GREGORIAN, gg.rules=(::GlobeGlitter::RULES_TIME_GREGORIAN).rules)
-      #assert_equal(::GlobeGlitter::RULES_RANDOM, gg.rules=(::GlobeGlitter::RULES_RANDOM).rules)
-      assert_raise(::ArgumentError) { gg.rules = 0 }
-      assert_raise(::ArgumentError) { gg.rules = 9 }
+      assert_equal(::GlobeGlitter::RULES_TIME_GREGORIAN, gg.replace_rules(::GlobeGlitter::RULES_TIME_GREGORIAN).rules)
+      assert_equal(::GlobeGlitter::RULES_RANDOM, gg.replace_rules(::GlobeGlitter::RULES_RANDOM).rules)
+      assert_raise(::ArgumentError) { gg.replace_rules(0) }
+      assert_raise(::ArgumentError) { gg.replace_rules(9) }
     }
   end
 
   def test_structure
     ::GlobeGlitter::nil.tap { |gg|
       assert_equal(::GlobeGlitter::STRUCTURE_UNSET, gg.structure)
-      #assert_equal(::GlobeGlitter::STRUCTURE_ITU_T_REC_X_667, gg.structure=(::GlobeGlitter::STRUCTURE_ITU_T_REC_X_667).structure)
-      #assert_equal(::GlobeGlitter::STRUCTURE_MICROSOFT, gg.structure=(::GlobeGlitter::STRUCTURE_MICROSOFT).structure)
-      #assert_equal(::GlobeGlitter::STRUCTURE_FUTURE, gg.structure=(::GlobeGlitter::STRUCTURE_FUTURE).structure)
-      assert_raise(::ArgumentError) { gg.structure = 4 }
+      assert_equal(::GlobeGlitter::STRUCTURE_ITU_T_REC_X_667, gg.replace_structure(::GlobeGlitter::STRUCTURE_ITU_T_REC_X_667).structure)
+      assert_equal(::GlobeGlitter::STRUCTURE_MICROSOFT, gg.replace_structure(::GlobeGlitter::STRUCTURE_MICROSOFT).structure)
+      assert_equal(::GlobeGlitter::STRUCTURE_FUTURE, gg.replace_structure(::GlobeGlitter::STRUCTURE_FUTURE).structure)
+      assert_raise(::ArgumentError) { gg.replace_structure(4) }
     }
   end
 
